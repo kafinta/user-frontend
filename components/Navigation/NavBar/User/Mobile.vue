37fa0182f6c5fe969800dd54089579d11c138fa2
Qaudri <aquadirmuhammad@gmail.com>
2023-02-27 14:01:41 +0100

    chore: mobile menu development in progress

--- components/Navigation/NavBar/User/Mobile.vue
+++ components/Navigation/NavBar/User/Mobile.vue
@@ -1,6 +1,36 @@
 <template>
-  <div :class="menu_revealed ? 'translate-x-0' : '-translate-x-full'" class="z-100 w-full fixed h-screen left-0 top-[65.5px] duration-500 ease-in-out bg-white md:hidden px-6">
-    <Accordion>
+  <div :class="menu_revealed ? 'translate-x-0' : '-translate-x-full'" class="z-100 w-full fixed h-screen left-0 top-[65.5px] duration-150 ease-in-out bg-white md:hidden px-6">
+    <Accordion>0
+
+      <AccordionItem container_class="py-3 px-2"
+        trigger_class="font-medium">
+        <template #accordion-trigger>
+          <h3>Account</h3>
+        </template>
+
+        <template #accordion-content>
+          <div class="py-3">
+            <ul v-if="!signedIn" class="flex flex-col gap-3">
+              <li>
+                <UiButtonsSecondary>Sign In</UiButtonsSecondary>
+              </li>
+              <li>
+                <UiButtonsPrimary>Sign Up</UiButtonsPrimary>
+              </li>
+            </ul>
+
+            <ul v-if="signedIn">
+              <li>
+                <UiButtonsSecondary>Profile</UiButtonsSecondary>
+              </li>
+              <li>
+                <UiButtonsSecondary>Dashboard</UiButtonsSecondary>
+              </li>
+            </ul>
+          </div>
+
+        </template>
+      </AccordionItem>
 
       <AccordionItem container_class="py-3 px-2"
         trigger_class="font-medium">
@@ -68,23 +98,6 @@
       <AccordionItem container_class="py-3 px-2"
         trigger_class="font-medium">
         <template #accordion-trigger>
-          <h3>Language</h3>
-        </template>
-
-        <template #accordion-content>
-          <div class="py-3">
-            <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-              ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-              aliquip ex ea commodo consequat.</span>
-          </div>
-
-        </template>
-      </AccordionItem>
-
-      <AccordionItem container_class="py-3 px-2"
-        trigger_class="font-medium">
-        <template #accordion-trigger>
           <h3>Tags</h3>
         </template>
 
@@ -106,7 +119,8 @@
 <script>
 export default {
   props: {
-    menu_revealed: false
+    menu_revealed: false,
+    signedIn: true
   }
 }
 </script>
