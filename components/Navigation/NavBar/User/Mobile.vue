5af09f2db470abedbc2be3013511f915f59b03ce
Qaudri <aquadirmuhammad@gmail.com>
2023-02-27 13:06:46 +0100

    chore: responsive menu design in progress

--- components/Navigation/NavBar/User/Mobile.vue
+++ components/Navigation/NavBar/User/Mobile.vue
@@ -1,45 +1,71 @@
 <template>
-  <nav :class="menu_revealed ? 'translate-x-0' : '-translate-x-full'" class="z-100 w-full fixed h-screen left-0 top-[65.5px] duration-500 ease-in-out bg-white md:hidden px-6">
+  <div :class="menu_revealed ? 'translate-x-0' : '-translate-x-full'" class="z-100 w-full fixed h-screen left-0 top-[65.5px] duration-500 ease-in-out bg-white md:hidden px-6">
     <Accordion>
 
-      <AccordionItem container_class=""
-        trigger_class="font-medium" active>
-
-        <template #accordion-trigger>
-          <h3>Format</h3>
-        </template>
-
-        <template #accordion-content>
-          <div class="py-3">
-            <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-              ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-              aliquip ex ea commodo consequat.</span>
-          </div>
-
-        </template>
-      </AccordionItem>
-
-      <AccordionItem container_class=""
-        trigger_class="font-medium" >
+      <AccordionItem container_class="py-3 px-2"
+        trigger_class="font-medium">
 
         <template #accordion-trigger>
           <h3>Categories</h3>
         </template>
 
         <template #accordion-content>
-          <div class="py-3">
-            <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-              ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-              aliquip ex ea commodo consequat.</span>
+          <div class="py-3 px-2">
+            <ul>
+              <li>
+                <UiButtonsSecondary>Link</UiButtonsSecondary>
+              </li>
+              <li>
+                <UiButtonsSecondary>Link</UiButtonsSecondary>
+              </li>
+              <li>
+                <UiButtonsSecondary>Link</UiButtonsSecondary>
+              </li>
+              <li>
+                <UiButtonsSecondary>Link</UiButtonsSecondary>
+              </li>
+              <li>
+                <UiButtonsSecondary>Link</UiButtonsSecondary>
+              </li>
+            </ul>
+          </div>
+
+        </template>
+      </AccordionItem>
+
+      <AccordionItem container_class="py-3 px-2"
+        trigger_class="font-medium" >
+
+        <template #accordion-trigger>
+          <h3>Professionals</h3>
+        </template>
+
+        <template #accordion-content>
+          <div class="py-3 px-2">
+            <ul>
+              <li>
+                <UiButtonsSecondary>Link</UiButtonsSecondary>
+              </li>
+              <li>
+                <UiButtonsSecondary>Link</UiButtonsSecondary>
+              </li>
+              <li>
+                <UiButtonsSecondary>Link</UiButtonsSecondary>
+              </li>
+              <li>
+                <UiButtonsSecondary>Link</UiButtonsSecondary>
+              </li>
+              <li>
+                <UiButtonsSecondary>Link</UiButtonsSecondary>
+              </li>
+            </ul>
 
           </div>
 
         </template>
       </AccordionItem>
 
-      <AccordionItem container_class=""
+      <AccordionItem container_class="py-3 px-2"
         trigger_class="font-medium">
         <template #accordion-trigger>
           <h3>Language</h3>
@@ -56,7 +82,7 @@
         </template>
       </AccordionItem>
 
-      <AccordionItem container_class=""
+      <AccordionItem container_class="py-3 px-2"
         trigger_class="font-medium">
         <template #accordion-trigger>
           <h3>Tags</h3>
@@ -73,8 +99,8 @@
         </template>
       </AccordionItem>
 
-</Accordion>
-  </nav>
+    </Accordion>
+  </div>
 </template>
 
 <script>
--- components/Navigation/NavBar/User/Search.vue
+++ components/Navigation/NavBar/User/Search.vue
@@ -1,52 +1,55 @@
 <template>
-  <nav :class="toggle_menu ? 'fixed z-100' : ''" class="flex items-center justify-between gap-5 px-6 md:px-8 lg:px-10 py-4 bg-white max-w-7xl mx-auto w-full">
-    <div>
-      <NavigationLogo class="w-36" @logoClicked="$router.push({path: '/'})"></NavigationLogo>
+  <nav class="fixed z-100 w-full">
+    <div class="flex items-center justify-between gap-5 px-6 md:px-8 lg:px-10 py-4 bg-white max-w-7xl mx-auto w-full">
+      <div>
+        <NavigationLogo class="w-36" @logoClicked="$router.push({path: '/'})"></NavigationLogo>
+      </div>
+
+      <NavigationMenu class="flex justify-end" @revealMenu="toggleMenu()" />
+
+      <form class="mx-auto w-full md:flex items-center justify-end relative hidden">
+        <input @focus="focus()" @input="$emit('input', $event.target.value)" class="w-full flex justify-center py-3 px-4 border text-sm outline-none border-secondary ring-0 focus:outline-none focus:border-primary active:border-primary rounded-md active:text-primary focus:text-primary text-secondary border-opacity-20 active:border-opacity-100 focus:border-opacity-100 duration-300 ease-out" autocomplete="off" ref="input" type="text" placeholder="What are you looking for?" />
+
+        <button class="bg-primary rounded-md p-3 absolute px-4">
+          <UiIconsSearch class="text-white w-5 h-6" />
+        </button>
+      </form>
+
+      <nav class="hidden md:block">
+        <ul class="flex gap-5 items-center justify-end" v-if="signedIn">
+          <li class="relative cursor-pointer">
+            <UiIconsNotifications class="w-4 h-4" />
+            <UiIconsPopup v-if="notifications" class="h-2 w-2 -top-0.5 right-0 absolute"></UiIconsPopup>
+          </li>
+          <li class="relative cursor-pointer">      
+            <UiIconsMessages class="w-4 h-4" />
+            <UiIconsPopup v-if="messages" class="h-2 w-2 -top-0.5 -right-1 absolute"></UiIconsPopup>
+          </li>
+          <li class="">
+            <UiButtonsSecondary class="w-[7.3rem] xl:w-[8.3rem] 2xl:w-[9.3rem]">Switch to Selling</UiButtonsSecondary>
+          </li>
+
+          <li class="relative cursor-pointer">
+            <slot />
+          </li>
+          <li v-if="earning" class="hidden">
+            <p class="text-primary border text-sm border-primary rounded-md px-2 py-1">${{earning}}</p>
+          </li>
+        </ul>
+
+        <ul class="flex gap-5 items-center justify-end" v-if="!signedIn">
+          <li>
+            <UiButtonsSecondary class="w-12 xl:w-14 2xl:w-16" @clicked="$router.push({path: '/login'})">Sign In</UiButtonsSecondary>
+          </li>
+          <li>
+            <UiButtonsPrimary class="w-[8.5rem]" @click="$router.push({path: '/signup'})">Sign Up</UiButtonsPrimary>
+          </li>
+        </ul>
+      </nav>
+
+      <NavigationNavBarUserMobile :menu_revealed="toggle_menu"></NavigationNavBarUserMobile>
     </div>
 
-    <NavigationMenu class="flex justify-end" @revealMenu="toggleMenu()" />
-
-    <form class="mx-auto w-full md:flex items-center justify-end relative hidden">
-      <input @focus="focus()" @input="$emit('input', $event.target.value)" class="w-full flex justify-center py-3 px-4 border text-sm outline-none border-secondary ring-0 focus:outline-none focus:border-primary active:border-primary rounded-md active:text-primary focus:text-primary text-secondary border-opacity-20 active:border-opacity-100 focus:border-opacity-100 duration-300 ease-out" autocomplete="off" ref="input" type="text" placeholder="What are you looking for?" />
-
-      <button class="bg-primary rounded-md p-3 absolute px-4">
-        <UiIconsSearch class="text-white w-5 h-6" />
-      </button>
-    </form>
-
-    <nav class="hidden md:block">
-      <ul class="flex gap-5 items-center justify-end" v-if="signedIn">
-        <li class="relative cursor-pointer">
-          <UiIconsNotifications class="w-4 h-4" />
-          <UiIconsPopup v-if="notifications" class="h-2 w-2 -top-0.5 right-0 absolute"></UiIconsPopup>
-        </li>
-        <li class="relative cursor-pointer">      
-          <UiIconsMessages class="w-4 h-4" />
-          <UiIconsPopup v-if="messages" class="h-2 w-2 -top-0.5 -right-1 absolute"></UiIconsPopup>
-        </li>
-        <li class="">
-          <UiButtonsSecondary class="w-[7.3rem] xl:w-[8.3rem] 2xl:w-[9.3rem]">Switch to Selling</UiButtonsSecondary>
-        </li>
-
-        <li class="relative cursor-pointer">
-          <slot />
-        </li>
-        <li v-if="earning" class="hidden">
-          <p class="text-primary border text-sm border-primary rounded-md px-2 py-1">${{earning}}</p>
-        </li>
-      </ul>
-
-      <ul class="flex gap-5 items-center justify-end" v-if="!signedIn">
-        <li>
-          <UiButtonsSecondary class="w-12 xl:w-14 2xl:w-16" @clicked="$router.push({path: '/login'})">Sign In</UiButtonsSecondary>
-        </li>
-        <li>
-          <UiButtonsPrimary class="w-[8.5rem]" @click="$router.push({path: '/signup'})">Sign Up</UiButtonsPrimary>
-        </li>
-      </ul>
-    </nav>
-
-    <NavigationNavBarUserMobile :menu_revealed="toggle_menu"></NavigationNavBarUserMobile>
   </nav>
 </template>
 
