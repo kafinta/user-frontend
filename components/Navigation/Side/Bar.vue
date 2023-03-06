589e27e203b6e13ba49013e1202149860ff46b99
Qaudri <aquadirmuhammad@gmail.com>
2023-03-06 09:22:02 +0100

    refactor: inverted logo component created for sidebar

--- components/Navigation/Side/Bar.vue
+++ components/Navigation/Side/Bar.vue
@@ -1,21 +1,18 @@
 <template>
   <transition name="slide-fade" >
-    <div class="fixed top-0 left-0 w-3/5 md:w-1/3 lg:w-1/5 xl:w-1/6 md:flex flex-col justify-between bg-secondary py-10 h-screen">
+    <nav class="fixed top-0 left-0 w-3/5 md:w-1/3 lg:w-1/5 xl:w-1/6 md:flex flex-col justify-between bg-secondary py-10 h-screen">
       <div class="px-6 md:px-10">
-        <NavigationLogo class="w-36 text-white"/>
+        <NavigationLogoInverted class="w-36 text-white"/>
       </div>
 
       <div class="my-10">
-
-        <div class="my-3">
-          <nuxt-link to="/">
-            <NavigationSideMenu>
-            </NavigationSideMenu>
-          </nuxt-link>
-        </div>
+        <nuxt-link to="/">
+          <NavigationSideMenu>
+          </NavigationSideMenu>
+        </nuxt-link>
 
       </div>
-    </div>
+    </nav>
   </transition>
 </template>
 
