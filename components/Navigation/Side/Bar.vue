941cbf1adc40cc12d71b4c733295df9f7056ac61
Qaudri <aquadirmuhammad@gmail.com>
2023-03-12 11:45:33 +0100

    refactor: sidebar component updated

--- components/Navigation/Side/Bar.vue
+++ components/Navigation/Side/Bar.vue
@@ -1,6 +1,6 @@
 <template>
   <transition name="slide-fade" >
-    <nav class="fixed z-100 top-0 left-0 w-3/5 md:w-1/3 lg:w-1/5 xl:w-1/6 flex flex-col justify-between bg-secondary h-screen">
+    <nav class="fixed z-100 top-0 left-0 w-4/6 md:w-1/3 lg:w-1/5 xl:w-1/6 flex flex-col justify-between bg-secondary h-screen">
       <div class="mt-6">
         <div class="px-8">
           <NavigationLogoInverted class="w-36 text-white"/>
