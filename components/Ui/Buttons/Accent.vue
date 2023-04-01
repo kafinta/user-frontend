eb3417819153ba0dfed46f0ec325cf5e66096570
Qaudri <aquadirmuhammad@gmail.com>
2023-04-01 10:40:40 +0100

    chore: new button component created

---
+++ components/Ui/Buttons/Accent.vue
@@ -0,0 +1,5 @@
+<template>
+  <button @click="$emit('clicked')" class="font-medium text-sm xl:text-base 2xl:text-lg text-secondary hover:text-primary ease-in-out duration-500 bg-accent-50 hover:bg-accent-100 select-none py-3 w-full focus:bg-accent-100 focus:text-primary outline-none">
+    <slot />
+  </button>
+</template>
