7fcfe5e38894cf39ed70c53e36fe51329db1f4d5
Qaudri <aquadirmuhammad@gmail.com>
2023-05-31 11:57:16 +0100

    chore: login function updated to use the backend

--- pages/login.vue
+++ pages/login.vue
@@ -15,7 +15,7 @@
     <div class="w-full lg:w-1/2 h-screen flex items-center justify-center mx-auto px-6 md:px-8 lg:px-10 relative">
       <NavigationLogo @click="returnHome()" class="absolute top-5 left-10 w-36 lg:hidden" :class="is_small ? 'hidden' : ''" />
 
-      <form @submit.prevent="signIn()" action="" class="w-full md:w-2/3 lg:w-full rounded-xl p-5" :class="is_small ? 'p-0' : ''">
+      <div @submit.prevent="signIn()" action="" class="w-full md:w-2/3 lg:w-full rounded-xl p-5" :class="is_small ? 'p-0' : ''">
         <h1 :class="is_small ? 'text-2xl' : ''" class="font-medium text-3xl w-fit text-secondary">Welcome back.</h1>
         <p :class="is_small ? 'mb-4' : ''" class="text-sm text-secondary mb-8">Enter your details to resume your session.</p>
 
@@ -38,24 +38,33 @@
           </div>
         </div>
 
-        <div class="grid gap-4">
+        <form class="grid gap-4">
           <FormInput label="Email" v-model="form.email" placeholder="Enter your email address"></FormInput>
-          <div>
+
+          <div class="w-full mb-5">
             <FormInput :error="error_state" label="Password" type="password" v-model="form.password" placeholder="Enter your password"></FormInput>
-            <nuxt-link to="/forgot" :class="error_state ? 'hidden' : 'block'" class="text-sm text-secondary text-opacity-50 flex justify-end w-full hover:text-opacity-100 duration-500 ease-in-out mt-2">Forgot password?</nuxt-link>
-            <p :class="error_state ? 'opacity-100' : 'opacity-0'" class="text-sm text-red-600 mt-2">Wrong username or password</p>
+
+            <div class="flex justify-between items-center mt-2 w-full">
+            
+              <p :class="error_state ? 'opacity-100' : 'opacity-0'" class="text-sm text-red-600 mt-2 duration-300 ease-in-out ">Wrong username or password</p>
+
+              <nuxt-link to="/forgot" :class="error_state ? 'hidden' : 'block'" class="text-sm text-secondary text-opacity-50 hover:text-opacity-100 duration-500 ease-in-out">Forgot password?</nuxt-link>
+
+            </div>
+
           </div>
 
-          <FormButton :loading="loadingState" class="-mt-3">Sign In</FormButton>
-        </div>
+          <FormButton :loading="loadingState">Sign In</FormButton>
+        </form>
         <p class="text-sm w-fit mx-auto mt-2 text-secondary text-center">Not a member yet? <nuxt-link to="/signup" class="duration-500 ease-in-out hover:text-primary">Create Account</nuxt-link></p>
-      </form>
+      </div>
     </div>
   </div>
 
 </template>
 
 <script>
+import {mapGetters, mapActions} from 'vuex';
 export default {
   data() {
     return {
@@ -70,17 +79,43 @@ export default {
   },
 
   methods: {
+    ...mapActions({
+      login : 'authentication/loginUser'
+    }),
+
+
     signIn(){
       this.loadingState = true
-      setTimeout(() => {
-        this.$router.push({path: '/sellers/'})
-      }, 2000);
-    },  },
+      this.login({
+        email: this.form.email,
+        password: this.form.password,
+      })
+      .then(() => {
+        this.$router.push({name: '_user-buying', params: {user: user_info.username}})
+        this.loadingState = false
+        console.log(user)
+      })
+      .catch(error => {
+        this.error_state = true
+        this.loadingState = false
+        this.form.password = ''
+        // this.$toast.error("Wrong username or password", {
+        //   duration: 2000,
+        // });
+      })
+    },  
+  },
   
   mounted(){
     if (window.innerWidth <= 320) {
       this.is_small = true
     }
+  },
+
+  computed: {
+    ...mapGetters({
+      user_info: "authentication/getUserInfo",
+    }),
   },
 }
 </script>
