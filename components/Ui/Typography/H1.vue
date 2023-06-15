6f23986b396fba614158f09c561c36d1a9c60726
Qaudri <aquadirmuhammad@gmail.com>
2023-06-15 16:00:40 +0100

    refactor: typography components redefined for colors

--- components/Ui/Typography/H1.vue
+++ components/Ui/Typography/H1.vue
@@ -1,10 +1,10 @@
 <template>
-  <h1 :class="is_white ? 'text-white' : 'text-secondary'" class="text-4xl md:text-5xl xl:text-6xl font-bold"><slot/></h1>
+  <h1 :class="color==='primary' ? 'text-primary' : color==='white' ? text-white :'text-secondary'" class="text-4xl md:text-5xl xl:text-6xl font-bold"><slot/></h1>
 </template>
 <script>
   export default {
     props: {
-      is_white: Boolean
+      color: String
     }
   }
 </script>
