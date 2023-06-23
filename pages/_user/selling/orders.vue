562151b18d3cda7c04b06d578b59cc62178cc9a3
Qaudri <aquadirmuhammad@gmail.com>
2023-06-23 15:51:00 +0100

    chore: orders page redesign in progress

--- pages/_user/selling/orders.vue
+++ pages/_user/selling/orders.vue
@@ -1,35 +1,66 @@
 <template>
   <LayoutsSellerDashboard page_title="Purchases Overview">
-    <div class="border border-accent-100 flex divide-x divide-accent-100 mt-6">
-      <div :class="tab.active ? 'bg-primary text-white' : 'hover:bg-secondary'" class="px-4 py-2 duration-500 ease-in-out group" v-for="tab in tabs" :key="tab.id">
-        <UiTypographyP :class="tab.active ? 'text-white' : 'text-secondary'" class="duration-500 ease-in-out group-hover:text-white">{{tab.title}}</UiTypographyP>
+    <div class="flex gap-5">
+      <div class="h-full hidden md:block w-80 lg:w-96 p-5">
+        <button @click="filter_revealed = true" class="border-0 p-0 flex gap-3 items-center mb-5">
+          <UiTypographyP>Filters</UiTypographyP>
+          <UiIconsFilter class="w-5 group-hover:text-secondary" />
+        </button>
+
+        <Accordion :class="filter_revealed ? 'block' : 'hidden md:block'" class="">
+          <AccordionItem container_class="border border-accent-100 py-3 px-5"
+            trigger_class="" active>
+            <template #accordion-trigger>
+              <UiTypographyP>Status</UiTypographyP>
+            </template>
+
+            <template #accordion-content>
+              <div class="gap-3 mt-3 grid">
+                <UiButtonsTab>Active</UiButtonsTab>
+                <UiButtonsTab>Draft</UiButtonsTab>
+              </div>
+            </template>
+          </AccordionItem>
+
+          <AccordionItem container_class="border border-accent-100 py-3 px-5 mt-3"
+            trigger_class="">
+            <template #accordion-trigger>
+              <UiTypographyP>Category</UiTypographyP>
+            </template>
+
+            <template #accordion-content>
+              <div class="gap-3 mt-3 grid">
+                <UiButtonsTab>Active</UiButtonsTab>
+                <UiButtonsTab>Draft</UiButtonsTab>
+              </div>
+            </template>
+          </AccordionItem>
+        </Accordion>
       </div>
-    </div>
-
-    <div class="border border-accent-100 py-2 px-4 w-full flex justify-between items-center mt-4" v-for="item in 5">
-      <div class="flex items-center gap-5">
-        <div class="relative flex w-fit">
-          <UserProfilePicture :artisan="username" :custom_dimensions="true" class="h-10 w-10"/>
-          <UserProfileOnlineStatus class="absolute right-0 bottom-0" :is_online="false" :is_displayed="true"/>
+      <div class="w-full">
+        <div class="grid-cols-5 hidden lg:grid">
+          <UiTypographyP>Product</UiTypographyP>
+          <UiTypographyP class="mx-auto">Status</UiTypographyP>
+          <UiTypographyP class="mx-auto">Purchase date</UiTypographyP>
+          <UiTypographyP class="mx-auto">Delivery date</UiTypographyP>
         </div>
-
-        <div>
-          <UiTypographyP class="text-secondary">{{ username }}</UiTypographyP>
+        <div class="hover:border-accent-200 lg:hover:border-accent-100 ease-in-out duration-500 cursor-pointer lg:cursor-default border border-accent-100 p-2 w-full grid lg:grid-cols-5 items-center mt-4" v-for="item in 5">
+          <div class="bg-accent-100 aspect-[3/2] w-1/2"></div>
+          <div class="mx-auto">
+            <UiTypographyP>In Progress</UiTypographyP>
+          </div>
+          <div class="hidden lg:block mx-auto">
+            <UiTypographyP>Date</UiTypographyP>
+          </div>
+          <div class="hidden lg:block mx-auto">
+            <UiTypographyP>Date</UiTypographyP>
+          </div>
+          <div class="hidden lg:flex justify-end">
+            <UiButtonsTertiary>View</UiButtonsTertiary>
+          </div>
         </div>
       </div>
 
-      <div class="flex gap-5 items-center">
-        <div>
-          <UiTypographyP :small_text="true">Price</UiTypographyP>
-          <UiTypographyP>$100</UiTypographyP>
-        </div>
-        <div>
-          <UiTypographyP :small_text="true">Status</UiTypographyP>
-          <UiTypographyP>In Progress</UiTypographyP>
-        </div>
-      </div>
-
-      <UiButtonsTertiary>View</UiButtonsTertiary>
     </div>
   </LayoutsSellerDashboard>
 </template>
@@ -38,23 +69,6 @@ export default {
   data() {
     return {
       username: 'Quadri',
-      tabs: [
-        {
-          id: 1,
-          title: 'Active',
-          active: true
-        },
-        {
-          id: 2,
-          title: 'Completed',
-          active: false
-        },
-        {
-          id: 3,
-          title: 'Cancelled',
-          active: false
-        },
-      ]
     }
   },
 }
