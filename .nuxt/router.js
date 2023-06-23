19710fa1ee7188882eba4d6825d7fb9d0fa16881
Qaudri <aquadirmuhammad@gmail.com>
2023-06-23 12:19:27 +0100

    chore: folder structure updated

--- .nuxt/router.js
+++ .nuxt/router.js
@@ -12,13 +12,13 @@ const _23a82a4a = () => interopDefault(import('..\\pages\\verify.vue' /* webpack
 const _68fb2dd0 = () => interopDefault(import('..\\pages\\marketplace\\search\\index.vue' /* webpackChunkName: "pages/marketplace/search/index" */))
 const _3e4b7391 = () => interopDefault(import('..\\pages\\index.vue' /* webpackChunkName: "pages/index" */))
 const _4fccbd46 = () => interopDefault(import('..\\pages\\_user\\buying\\index.vue' /* webpackChunkName: "pages/_user/buying/index" */))
+const _2551ce90 = () => interopDefault(import('..\\pages\\_user\\profile.vue' /* webpackChunkName: "pages/_user/profile" */))
+const _5e6ef04c = () => interopDefault(import('..\\pages\\_user\\settings.vue' /* webpackChunkName: "pages/_user/settings" */))
 const _76c7efa6 = () => interopDefault(import('..\\pages\\_user\\selling\\dashboard.vue' /* webpackChunkName: "pages/_user/selling/dashboard" */))
 const _60d859a0 = () => interopDefault(import('..\\pages\\_user\\selling\\earnings.vue' /* webpackChunkName: "pages/_user/selling/earnings" */))
 const _4fd79f9f = () => interopDefault(import('..\\pages\\_user\\selling\\inbox.vue' /* webpackChunkName: "pages/_user/selling/inbox" */))
 const _6e194d9c = () => interopDefault(import('..\\pages\\_user\\selling\\orders.vue' /* webpackChunkName: "pages/_user/selling/orders" */))
 const _748a7f47 = () => interopDefault(import('..\\pages\\_user\\selling\\products\\index.vue' /* webpackChunkName: "pages/_user/selling/products/index" */))
-const _68b93dfc = () => interopDefault(import('..\\pages\\_user\\selling\\profile.vue' /* webpackChunkName: "pages/_user/selling/profile" */))
-const _fb60a7cc = () => interopDefault(import('..\\pages\\_user\\selling\\settings.vue' /* webpackChunkName: "pages/_user/selling/settings" */))
 const _05b90756 = () => interopDefault(import('..\\pages\\_user\\selling\\products\\new.vue' /* webpackChunkName: "pages/_user/selling/products/new" */))
 
 const emptyFn = () => {}
@@ -65,6 +65,14 @@ export const routerOptions = {
     component: _4fccbd46,
     name: "user-buying"
   }, {
+    path: "/:user/profile",
+    component: _2551ce90,
+    name: "user-profile"
+  }, {
+    path: "/:user/settings",
+    component: _5e6ef04c,
+    name: "user-settings"
+  }, {
     path: "/:user/selling/dashboard",
     component: _76c7efa6,
     name: "user-selling-dashboard"
@@ -84,14 +92,6 @@ export const routerOptions = {
     path: "/:user/selling/products",
     component: _748a7f47,
     name: "user-selling-products"
-  }, {
-    path: "/:user/selling/profile",
-    component: _68b93dfc,
-    name: "user-selling-profile"
-  }, {
-    path: "/:user/selling/settings",
-    component: _fb60a7cc,
-    name: "user-selling-settings"
   }, {
     path: "/:user/selling/products/new",
     component: _05b90756,
