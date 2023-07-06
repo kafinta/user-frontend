704f91b1491356bde86f6cf8e00e82366d06f8b7
Qaudri <aquadirmuhammad@gmail.com>
2023-07-06 11:24:50 +0100

    chore: earning breakdown design outline completed

--- pages/_user/selling/earnings.vue
+++ pages/_user/selling/earnings.vue
@@ -9,6 +9,22 @@
         </div>
       </div>
     </div>
+
+    <div class="mt-6">
+      <div class="grid grid-cols-4 py-2 gap-2 justify-between px-10">
+        <UiTypographyP class="">Amount</UiTypographyP>
+        <UiTypographyP class="mx-auto">Clearance Date</UiTypographyP>
+        <UiTypographyP class="mx-auto">Product</UiTypographyP>
+      </div>
+      <div class="py-2 px-10 border-accent-100 border grid grid-cols-4 items-center mb-2" v-for="item in 5">
+        <UiTypographyP class="">$0.00</UiTypographyP>
+        <UiTypographyP class="mx-auto">00/00/0000</UiTypographyP>
+        <div class="bg-accent-100 w-12 aspect-[3/2] mx-auto"></div>
+        <div class="flex justify-end">
+          <UiButtonsTertiary>View Order</UiButtonsTertiary>
+        </div>
+      </div>
+    </div>
   </LayoutsSellerDashboard>
 </template>
 <script>
