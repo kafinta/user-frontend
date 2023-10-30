e6b4b6c3559319adf589a08339dc83dc35b94bd3
Qaudri <aquadirmuhammad@gmail.com>
2023-10-30 12:31:31 +0100

    marketplace layout updated

--- components/layouts/Marketplace.vue
+++ components/layouts/Marketplace.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <NavigationNavBarUserSearch/>
-    <div class="mt-28 px-6 md:px-8 lg:px-10 max-w-7xl mx-auto mb-8">
+    <div class="mt-28 px-6 md:px-8 lg:px-10 mb-8">
       <slot/>
     </div>
     <NavigationFooter/>
