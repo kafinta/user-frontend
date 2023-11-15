3cf51aafa2c7f845d54f764cec304d252a3caa04
Quadir <aquadirmuhammad@gmail.com>
2023-11-15 18:59:37 +0100

    getCategories composable function written

--- composables/useCategories.ts
+++ composables/useCategories.ts
@@ -1,5 +1,17 @@
-// export function useCategories<T>(count: number) {
+export function useCategories<T>() {
+  const getSomeCategories = async () => {
+    const { data: csrf_token_data, error: csrf_token_error } = await useCustomFetch('/sanctum/csrf-cookie')
+  
+    const { pending, data: user_auth_data, error: user_auth_error } = await useCustomFetch('api/categories/7', {
+      method: 'GET',
+      onResponse(res) {
+        if (res.response.status == 200) {
+          const thecategories = useState('categories', () => res.response._data)
+          const categories = thecategories.value
+        }
+      },
+    })
+  };
 
-
-//   return useCategories(count)
-// }
+  return useCategories()
+}
