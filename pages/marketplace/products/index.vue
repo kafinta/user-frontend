e2ff673c52613ba3a81b66ae1fa5b46fb0131e71
Quadir <aquadirmuhammad@gmail.com>
2023-11-15 19:00:57 +0100

    products listing page design in progress

--- pages/marketplace/products/index.vue
+++ pages/marketplace/products/index.vue
@@ -1,5 +1,14 @@
 <template>
-  <div>
-    
-  </div>
+  <LayoutsMarketplace>
+    <div class="grid grid-cols-1 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
+      <aside>
+
+      </aside>
+      <ul>
+        <li v-for="item in 30"></li>
+      </ul>
+    </div>
+  </LayoutsMarketplace>
 </template>
+<script lang="ts" setup>
+</script>
