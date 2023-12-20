bb6607a559b0437b6eade5ddf11985da4b23d717
Quadir <aquadirmuhammad@gmail.com>
2023-12-20 17:13:48 +0100

    subcategories added to filter

--- components/Filter/Index.vue
+++ components/Filter/Index.vue
@@ -9,12 +9,30 @@
         </template>
 
         <template #accordion-content content_class="px-3 py-2">
-          <div v-if="categoryLoaded" class="mt-3 flex gap-2 flex-wrap text-left">
-            <UiButtonsTertiary @clicked="query.category = category.name; $router.push({name: '', query}); selectCategory(category.name)" class="text-left text-secondary hover:text-primary py-2 duration-500 ease-in-out" v-for="category in categories" :key="category.id">{{category.name}}</UiButtonsTertiary>
+          <div :class="toggleSubcategories ? 'hidden' : 'block'" class="mt-3">
+            <ul v-if="categoryLoaded" class="flex gap-2 flex-wrap text-left">
+              <li v-for="category in categories" :key="category.id">
+                <UiButtonsTertiary @clicked="query.category = category.name; $router.push({name: '', query}); selectCategory(category)" class="text-left text-secondary hover:text-primary py-2 duration-500 ease-in-out">{{category.name}}</UiButtonsTertiary>
+              </li>
+            </ul>
+            <div v-else class="flex items-center justify-center">
+              <UiIconsLoading class="text-accent-100 h-10 w-10" />
+            </div>
           </div>
 
-          <div v-else class="flex items-center justify-center">
-            <UiIconsLoading class="text-accent-100 h-10 w-10" />
+          <div :class="toggleSubcategories ? 'block' : 'hidden'" class="mt-3">
+            <ul v-if="subcategoryLoaded" class="flex gap-2 flex-wrap text-left">
+              <li v-for="subcategory in subcategories" :key="subcategory.id">
+                <UiButtonsTertiary @clicked="query.subcategory = subcategory.name; $router.push({name: '', query}); selectCategory(subcategory)" class="text-left text-secondary hover:text-primary py-2 duration-500 ease-in-out">{{subcategory.name}}</UiButtonsTertiary>
+              </li>
+            </ul>
+            <div v-else class="flex items-center justify-center">
+              <UiIconsLoading class="text-accent-100 h-10 w-10" />
+            </div>
+          </div>
+
+          <div :class="toggleSubcategories ? 'flex' : 'hidden'" class="mt-3 flex gap-2 flex-wrap text-left">
+
           </div>
         </template>
       </AccordionItem>
@@ -26,7 +44,7 @@
         </template>
 
         <template #accordion-content content_class="px-3 py-2">
-          <div v-if="categoryLoaded" class="mt-3 flex gap-2 flex-wrap text-left">
+          <div v-if="locationsLoaded" class="mt-3 flex gap-2 flex-wrap text-left">
             <UiButtonsTertiary @clicked="query.location = location.name; $router.push({name: '', query}); console.log($route.query)" class="text-left text-secondary hover:text-primary py-2 duration-500 ease-in-out" v-for="location in locations" :key="location.id">{{location.name}}</UiButtonsTertiary>
           </div>
 
@@ -35,7 +53,6 @@
           </div>
         </template>
       </AccordionItem>
-
       <AccordionItem container_class="border border-accent lg:border-none"
         trigger_class=" border-b border-accent py-2 px-3">
         <template #accordion-trigger >
@@ -78,12 +95,14 @@ const route = useRoute()
 const router = useRouter()
 
 const categoryLoaded = ref(false)
+const subcategoryLoaded = ref(false)
 const locationsLoaded = ref(false)
+const toggleSubcategories = ref(false)
 let locations = []
 let categories = []
+let subcategories = []
 const query = ref({})
 const getCategories = async () => {
-  const { data: csrf_token_data, error: csrf_token_error } = await useCustomFetch('/sanctum/csrf-cookie')
   const { pending, data: user_auth_data, error: user_auth_error } = await useCustomFetch('api/categories/', {
     method: 'GET',
     onResponse(res) {
@@ -96,13 +115,26 @@ const getCategories = async () => {
   })
 };
 
+const selectCategory = async (category) => {
+  const { pending, data, error } = await useCustomFetch(`api/categories/${category.id}/subcategories/`, {
+    method: 'GET',
+    onResponse(res) {
+      console.log(res.response)
+      if (res.response.status == 200) {
+        subcategories = useState('subcategories', () => res.response._data.subcategories).value
+        subcategoryLoaded.value = true
+        toggleSubcategories.value = true
+      }
+    },
+  })
+}
+
 const getLocations = async () => {
-  const { data: csrf_token_data, error: csrf_token_error } = await useCustomFetch('/sanctum/csrf-cookie')
-  const { pending, data: user_auth_data, error: user_auth_error } = await useCustomFetch('api/locations/', {
+  const { pending, data: locationData, error: user_auth_error } = await useCustomFetch('api/locations/', {
     method: 'GET',
     onResponse(res) {
       if (res.response.status == 200) {
-        locations = useState('locations', () => res.response._data).value
+        locations = useState('locations', () => locationData).value
         locationsLoaded.value = true
       }
     },
