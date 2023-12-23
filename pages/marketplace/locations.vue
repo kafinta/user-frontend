0f852b96d2de6c275c06e82e58fe2bc504037b90
Quadir <aquadirmuhammad@gmail.com>
2023-12-23 15:57:17 +0100

    query implemented on locations

--- pages/marketplace/locations.vue
+++ pages/marketplace/locations.vue
@@ -2,7 +2,7 @@
   <LayoutsMarketplace>
     <ul class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
       <li v-if="locationsLoaded" v-for="item in locations" :key="item.id">
-        <UiCards @clicked="$router.push({name: 'marketplace-products', query: {location: item.name}})" :title="item.name" :backgroundImagePath="'http://localhost:8000' + item.image" class="w-full"/>
+        <UiCards @clicked="query.location=item.name; $router.push({name: 'marketplace-products', query})" :title="item.name" :backgroundImagePath="'http://localhost:8000' + item.image" class="w-full"/>
       </li>
       <li v-else class="flex items-center justify-center">
         <UiIconsLoading class="text-primary h-10 w-10" />
@@ -13,7 +13,8 @@
 </template>
 <script setup>
 import { onMounted, ref } from "vue";
-
+import { useQuery } from "@/composables/useQuery";
+const {query} = useQuery() 
 const locationsLoaded = ref(false)
 let locations = []
 
