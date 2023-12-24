ec35cbb67b47e432b99fa983a343c111c0bfa2e7
Quadir <aquadirmuhammad@gmail.com>
2023-12-24 19:04:00 +0100

    product viewing page updated

--- pages/marketplace/products/[product].vue
+++ pages/marketplace/products/[product].vue
@@ -1,3 +1,15 @@
 <template>
-  <div></div>
+  <LayoutsMarketplace>
+    <div class="flex gap-16">
+      <div class="w-3/5">
+        <div class="bg-accent-100 w-full aspect-[3/2]"></div>
+        <UiTypographyP>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ea odio facilis aliquam adipisci laboriosam fugiat nulla inventore aperiam. Explicabo nulla, quidem voluptate iste vitae sapiente ratione quas ut magnam illo. Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus atque distinctio repudiandae quae architecto accusamus quas modi recusandae, repellat quidem. Debitis tenetur cupiditate nihil odio. Facilis repellendus id nobis libero. Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel vitae veritatis animi maiores, illo quibusdam perspiciatis vero temporibus quae, doloribus adipisci blanditiis quasi architecto officiis saepe maxime doloremque expedita hic!</UiTypographyP>
+      </div>
+
+      <div class="sticky top-20">
+        <UiTypographyH2>Product Name</UiTypographyH2>
+        <UiTypographyP></UiTypographyP>
+      </div>
+    </div>
+  </LayoutsMarketplace>
 </template>
