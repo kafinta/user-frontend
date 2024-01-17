0ca385055c7330a96790448c3adf4444dc659589
Quadir <aquadirmuhammad@gmail.com>
2024-01-17 09:37:21 +0100

    button components updated

--- components/Ui/Buttons/Secondary.vue
+++ components/Ui/Buttons/Secondary.vue
@@ -1,5 +1,5 @@
 <template>
-  <button @click="$emit('clicked')" class="font-medium text-sm xl:text-base 2xl:text-lg text-secondary hover:text-primary ease-in-out duration-500 border-b border-transparent hover:border-primary select-none text-left outline-none">
+  <button @click="$emit('clicked')" class="font-medium text-sm xl:text-base 2xl:text-lg text-secondary hover:text-primary ease-in-out duration-500 border-b border-transparent hover:border-primary select-none outline-none">
     <slot />
   </button>
 </template>
--- components/Ui/Buttons/Tertiary.vue
+++ components/Ui/Buttons/Tertiary.vue
@@ -2,18 +2,8 @@
   <button 
     @click="$emit('clicked')"
     class="py-2 px-5 text-secondary font-medium text-sm lg:text-base 2xl:text-lg justify-center duration-500 ease-in-out rounded-md active:text-secondary border hover:text-primary hover:border-primary focus:border-primary focus:text-primary outline-none" 
-    :disabled="loading || disabled" :class="[flexdisplay ? 'flex w-full text-center' : 'block w-fit text-left']">
-    <div class="flex items-center justify-center">
-      <div v-if="loading"  class="flex items-center">
-        <div class=" mr-2">
-          <UiIconsLoading class="w-7"></UiIconsLoading>
-        </div>
-        {{ loadingText }}
-      </div>
-      <div v-if="!loading">
-        <slot />
-      </div>
-    </div>
+    :class="[flexdisplay ? 'flex w-full text-center gap-3 items-center' : 'w-fit text-left']">
+    <slot />
   </button>
 </template>
 
@@ -25,16 +15,6 @@ export default {
       default: false,
       type: Boolean
     },
-    loading: Boolean,
-    color: String,
-    disabled: Boolean,
-    loadingText: {
-      default: "Loading",
-      type: String
-    },
   },
 }
 </script>
-
-<style>
-</style>
