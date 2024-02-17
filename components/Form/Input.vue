387658720f7b9ed8afe1e0461a4102bf6f9372ce
Quadir <aquadirmuhammad@gmail.com>
2024-02-17 13:35:23 +0100

    responsiveness updated on profile page, v-model added

--- components/Form/Input.vue
+++ components/Form/Input.vue
@@ -4,7 +4,6 @@
       <input @input="$emit('update:inputValue', $event.target.value)" @focus="focus()" :class="{'border-red-600 focus:border-red-600 active:border-red-600' : error, 'text-center placeholder-center' : centerText, }"  class="w-full py-3 px-4 border text-sm outline-none border-secondary ring-0 focus:outline-none focus:border-primary active:border-primary rounded-md active:text-primary focus:text-primary text-secondary border-opacity-20 active:border-opacity-100 focus:border-opacity-100 duration-300 ease-out bg-white" autocomplete="off"
     :type="type" :disabled="disabled" :placeholder="placeholder" :value="inputValue" ref="input" :id="label">
     </div>
-    
 </template>
 
 <script>
--- pages/[username]/selling/onboarding/profile.vue
+++ pages/[username]/selling/onboarding/profile.vue
@@ -1,29 +1,24 @@
-
-import { FormInput } from '#build/components';
 <template>
   <LayoutsSellerDashboard page_title="Onboarding">
-    <main class="h-full w-full grid place-items-center">
-      <div class="w-full lg:w-2/3 2xl:w-1/2 mx-auto">
-        <UiTypographyH3 class="text-center">Profile Registration</UiTypographyH3>
-
-        <form action="" class="grid grid-cols-1 lg:grid-cols-2 gap-6 w-full mt-6">
-          <div class="mx-auto lg:col-span-2 relative flex items-center justify-center">
-            <input @change="previewImage($event)" type="file" id="profilePictureInput" class="hidden" accept="image/*">
-            <label for="profilePictureInput" class="group h-52 aspect-square rounded-full bg-accent-500 bg-opacity-[10%] hover:bg-opacity-50 duration-500 ease-in-out grid place-items-center cursor-pointer absolute">
-              <img src="/images/icons/camera.svg" class="w-20 opacity-50" alt="" />
-            </label>
-            <img :src="previewSrc" ref="preview" class="h-52 aspect-square rounded-full object-cover">
-          </div>
-          <FormInput label="First name" type="text"></FormInput>
-          <FormInput label="Last name" type="text"></FormInput>
-          <FormInput label="Nationality" type="text"></FormInput>
-          <FormInput label="Phone Number" type="phone"></FormInput>
-          <FormTextarea type="tel" label="Description" placeholder="Write a few words to describe your brand/company." class="lg:col-span-2" ></FormTextarea>
-          <div class="col-span-2">
-            <FormButton  class="max-w-64 mx-auto">Create Profile</FormButton>
-          </div>
-        </form>
-      </div>
+    <main class="w-full lg:w-2/3 2xl:w-1/2 mx-auto">
+      <UiTypographyH3 class="text-center">Profile Registration</UiTypographyH3>
+      <form action="" class="flex flex-col lg:grid lg:grid-cols-2 gap-6 w-full mt-6">
+        <div class="col-span-1 lg:col-span-2 relative flex items-center justify-center">
+          <input @change="previewImage($event)" type="file" id="profilePictureInput" class="hidden" accept="image/*" :v-model="form.profile_picture">
+          <label for="profilePictureInput" class="group h-52 aspect-square rounded-full bg-accent-500 bg-opacity-[10%] hover:bg-opacity-50 duration-500 ease-in-out grid place-items-center cursor-pointer absolute">
+            <img src="/images/icons/camera.svg" class="w-20 opacity-50" alt="" />
+          </label>
+          <img :src="previewSrc" ref="preview" class="h-52 aspect-square rounded-full object-cover">
+        </div>
+        <FormInput label="First name" type="text" v-model:inputValue="form.first_name"></FormInput>
+        <FormInput label="Last name" type="text" v-model:inputValue="form.last_name"></FormInput>
+        <FormInput label="Nationality" type="text" v-model:inputValue="form.nationality"></FormInput>
+        <FormInput label="Phone Number" type="phone" v-model:inputValue="form.phone"></FormInput>
+        <FormTextarea type="tel" label="Description" v-model:inputValue="form.description" placeholder="Write a few words to describe your brand/company." class="lg:col-span-2" ></FormTextarea>
+        <div class="col-span-2">
+          <FormButton class="max-w-64 mx-auto">Create Profile</FormButton>
+        </div>
+      </form>
     </main>
   </LayoutsSellerDashboard>
 </template>
@@ -31,8 +26,15 @@ import { FormInput } from '#build/components';
 export default {
   data() {
     return {
-      previewSrc: '', // Initial empty value for preview
-      previewLoaded : false
+      previewSrc: '',
+      form: {
+        first_name: '',
+        last_name: '',
+        description: '',
+        nationality: '',
+        phone: '',
+        profile_picture: '',
+      }
     };
   },
   methods: {
@@ -53,13 +55,5 @@ export default {
       }
     }
   },
-  mounted(){
-
-  }
 }
 </script>
-<style>
-.preview_image{
-  background: url(`${this.previewSrc}`);
-}
-</style>
