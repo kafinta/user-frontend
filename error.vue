eaa5c345ce269ed56c7d94e0a29fe04d699b64ae
Quadir <aquadirmuhammad@gmail.com>
2024-02-20 16:20:22 +0100

    page errors handled

---
+++ error.vue
@@ -0,0 +1,20 @@
+<template>
+  <main class="h-screen grid place-items-center">
+    <div>
+      <h1 class="text-9xl text-center font-bold">{{error.statusCode}}!</h1>
+      <p>
+        {{ error.message }}
+      </p>
+
+      <UiButtonsPrimary @clicked="handleError" class="mt-10 mx-auto">Go back home</UiButtonsPrimary>
+    </div>
+  </main>
+</template>
+<script setup>
+const error = useError();
+const handleError = () => {
+  clearError({
+    redirect: '/',
+  });
+};
+</script>
