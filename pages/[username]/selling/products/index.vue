f5c1c9e1c91bb1b05b9f18569e9adf1148b96f1e
Quadir <aquadirmuhammad@gmail.com>
2024-02-29 14:06:32 +0100

    new product page design in progress

--- pages/[username]/selling/products/index.vue
+++ pages/[username]/selling/products/index.vue
@@ -1,49 +1,9 @@
 <template>
   <LayoutsSellerDashboard page_title="Products Overview">
     <div class="md:flex block w-full gap-5">
-      <div class="h-full hidden md:block w-80 lg:w-96 p-5">
-        <button @click="filter_revealed = true" class="border-0 p-0 flex gap-3 items-center mb-5">
-          <UiTypographyP>Filters</UiTypographyP>
-          <UiIconsFilter class="w-5 group-hover:text-secondary" />
-        </button>
-
-        <Accordion :class="filter_revealed ? 'block' : 'hidden md:block'" class="">
-          <AccordionItem container_class="border border-accent-100 py-3 px-5"
-            trigger_class="" active>
-            <template #accordion-trigger>
-              <UiTypographyP>Status</UiTypographyP>
-            </template>
-
-            <template #accordion-content>
-              <div class="gap-3 mt-3 grid">
-                <UiButtonsTab>Active</UiButtonsTab>
-                <UiButtonsTab>Draft</UiButtonsTab>
-              </div>
-            </template>
-          </AccordionItem>
-
-          <AccordionItem container_class="border border-accent-100 py-3 px-5 mt-3"
-            trigger_class="">
-            <template #accordion-trigger>
-              <UiTypographyP>Category</UiTypographyP>
-            </template>
-
-            <template #accordion-content>
-              <div class="gap-3 mt-3 grid">
-                <UiButtonsTab>Active</UiButtonsTab>
-                <UiButtonsTab>Draft</UiButtonsTab>
-              </div>
-            </template>
-          </AccordionItem>
-        </Accordion>
-      </div>
       <div class="w-full">
         <div class="flex justify-between items-center w-full">
-          <UiButtonsTab @clicked="filter_drawer = true" class="md:hidden flex gap-3 items-center">
-            <p class="text-base">Filters</p>
-            <UiIconsFilter class="w-5 group-hover:text-secondary" />
-          </UiButtonsTab>
-          <UiTypographyP class="hidden md:block">My Products</UiTypographyP>
+          <UiTypographyP>My Products</UiTypographyP>
           <UiButtonsPrimary @clicked="$router.push({path: 'products/new'})">List a New Product</UiButtonsPrimary>
         </div>
         <ul v-if="products" class="grid lg:grid-cols-2 gap-6 place-items-center mt-6">
