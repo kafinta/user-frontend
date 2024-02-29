04db55d58551e829a6e4c1445f008ef1443d3be6
Quadir <aquadirmuhammad@gmail.com>
2024-02-29 12:23:08 +0100

    dialog component updated

--- components/Modals/Overlay.vue
+++ components/Modals/Overlay.vue
@@ -6,29 +6,17 @@
     </transition>
     
     <transition name="slide-fade">
-      <div v-show="openOverlay" class="fixed h-auto w-full sm:w-2/3 lg:w-3/5 xl:w-1/3 bg-white z-150 top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2">
+      <div v-show="openOverlay" class="fixed h-auto w-full sm:w-2/3 lg:w-3/5 xl:w-1/3 bg-white z-150 top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 rounded-md">
         <div class="relative">
-          <div class="flex justify-between items-center border-b border-accent1-100 py-3 sm:py-4 sm:px-2 select-none">
-            <p class="uppercase font-bold px-4 select-none"><slot name="title" /></p>
+          <div class="flex justify-between items-center border-b border-accent1-100 py-3 sm:py-4 px-4 select-none">
+            <p class="uppercase font-bold select-none"><slot name="title" /></p>
             <button @click="emitCloseDialogEvent()" class="block outline-none border-none p-1 cursor-pointer hover:bg-primary hover:text-white text-secondary bg-accent-100 transition duration-300">
-              <UiIconsClose class="h-5 w-5 " />
+              <UiIconsClose class="h-5 w-5 "/>
             </button>
           </div>
           
-          <div class="p-4 md:p-6" :class="(scrollable ? 'overflow-y-scroll' : 'overflow-hidden')">
+          <div class="p-4 md:p-6 grid gap-6 text-center place-items-center">
             <slot />
-          </div>
-          
-          <div v-show="footerButtons" class="absolute bottom-0 w-full select-none">
-            <div class="w-full flex justify-between">
-              <button @click="emitCloseDialogEvent()" class="uppercase font-medium border-t border-r border-gray-400 shadow rounded-bl-xl w-1/2 block px-2 py-3 text-center cursor-pointer bg-white hover:bg-gray-200 transition duration-500">
-                {{cancelText}}
-              </button>
-              <button @click="emitOkEvent()" class="uppercase font-medium border-t border-accent1-300 shadow rounded-br-xl w-1/2 block px-2 py-3 text-center cursor-pointer bg-accent1-100 hover:bg-accent1-200 transition duration-500">
-                {{okText}}
-              </button>
-            </div>
-            
           </div>
         </div>
 
@@ -40,34 +28,15 @@
 
 <script>
 export default {
-  name: "Side",
+  name: "Dialog",
   props: {
     openOverlay: Boolean,
-    footerButtons: {
-      type: Boolean,
-      default: false,
-    },
-    scrollable: {
-      type: Boolean,
-      default: false,
-    },
-    cancelText: {
-      type: String,
-      default: 'Cancel'
-    },
-    okText: {
-      type: String,
-      default: 'Ok'
-    }
   },
 
   methods: {
     emitCloseDialogEvent(){
       this.$emit('closeOverlay')
     },
-    emitOkEvent(){
-      this.$emit('continueAction')
-    }
   }
 
 }
