b7ff188294539be2ccbb4e6610607df31ff15b4c
Quadir <aquadirmuhammad@gmail.com>
2024-03-06 16:39:54 +0100

    profile page updated:

--- components/Navigation/Side/Bar/Buying.vue
+++ components/Navigation/Side/Bar/Buying.vue
@@ -47,11 +47,11 @@
         </div>
 
         <div class="mt-2 flex gap-2 w-full">
-          <nuxt-link to="/sellers/profile" class="block rounded-md p-2 text-white hover:text-primary duration-500 ease-in-out group bg-secondary hover:bg-accent-500 w-1/2">
+          <nuxt-link to="/username/profile" class="block rounded-md p-2 text-white hover:text-primary duration-500 ease-in-out group bg-secondary hover:bg-accent-500 w-1/2">
             <UiIconsProfile class="w-5 h-5 mx-auto" />
             <p class="text-white group-hover:text-primary duration-500 ease-in-out text-sm text-center mt-1">Profile</p>
           </nuxt-link>
-          <nuxt-link to="/sellers/settings" class="block rounded-md p-2 text-white hover:text-primary duration-500 ease-in-out group bg-secondary hover:bg-accent-500 w-1/2">
+          <nuxt-link to="/username/settings" class="block rounded-md p-2 text-white hover:text-primary duration-500 ease-in-out group bg-secondary hover:bg-accent-500 w-1/2">
             <UiIconsSettings class="w-5 h-5 mx-auto" />
             <p class="text-white group-hover:text-primary duration-500 ease-in-out text-sm text-center mt-1">Settings</p>
           </nuxt-link>
