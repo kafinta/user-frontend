cdb0b0dfb2d460a862937966d9cb7209df02ce48
Quadir <aquadirmuhammad@gmail.com>
2024-04-02 11:55:51 +0100

    filter updated to use store

--- components/Filter/Index.vue
+++ components/Filter/Index.vue
@@ -2,124 +2,121 @@
   <aside class="lg:border-r lg:border-accent lg:pr-2">
     <UiTypographyP class="mb-2 hidden lg:block">Filters</UiTypographyP>
     <Accordion class="grid gap-5">
-      <AccordionItem container_class="border border-accent-100 p-2 px-3 rounded-md"
-        trigger_class="font-medium text-lg" active>
-        <template #accordion-trigger >
-          <h4>Category</h4>
-        </template>
 
-        <template #accordion-content content_class="px-3 py-2">
-          <div :class="toggleSubcategories ? 'hidden' : 'block'" class="mt-3">
-            <ul v-if="categoryLoaded" class="flex gap-2 flex-wrap text-left">
-              <li v-for="category in categories" :key="category.id">
-                <UiButtonsTertiary @clicked="selectCategory(category); query.category = category.name; $router.push({name: '', query});" class="text-left text-secondary hover:text-primary py-2 duration-500 ease-in-out">{{category.name}}</UiButtonsTertiary>
-              </li>
-            </ul>
-            <div v-else class="flex items-center justify-center">
-              <UiIconsLoading class="text-accent-100 h-10 w-10" />
-            </div>
-          </div>
+    <AccordionItem container_class="p-2 px-3 rounded-md border border-accent-100"
+      trigger_class="font-medium text-lg">
 
-          <div :class="toggleSubcategories ? 'block' : 'hidden'" class="mt-3 px-5">
-            <UiButtonsSecondary @clicked="toggleSub()" class="flex gap-2 items-center mb-5">
-              <span><UiIconsArrow class="w-4" /></span>
-              {{ categoryName }}
-            </UiButtonsSecondary>
-            <ul v-if="subcategoryLoaded" class="flex gap-2 flex-wrap text-left">
-              <li v-for="subcategory in subcategories" :key="subcategory.id">
-                <UiButtonsTertiary @clicked="query.subcategory = subcategory.name; $router.push({name: '', query}); selectCategory(subcategory)" class="text-left text-secondary hover:text-primary py-2 duration-500 ease-in-out">{{subcategory.name}}</UiButtonsTertiary>
-              </li>
-            </ul>
-            <div v-else class="flex items-center justify-center">
-              <UiIconsLoading class="text-accent-100 h-10 w-10" />
-            </div>
-          </div>
+      <template #accordion-trigger>
+        <h4>Categories</h4>
+      </template>
 
-          <div :class="toggleSubcategories ? 'flex' : 'hidden'" class="mt-3 flex gap-2 flex-wrap text-left">
-
-          </div>
-        </template>
-      </AccordionItem>
-
-      <AccordionItem container_class="border border-accent-100 p-2 px-3 rounded-md"
-        trigger_class="font-medium text-lg">
-        <template #accordion-trigger >
-          <h4>Locations</h4>
-        </template>
-
-        <template #accordion-content content_class="px-3 py-2">
-          <div v-if="locationsLoaded" class="mt-3 flex gap-2 flex-wrap text-left">
-            <UiButtonsTertiary @clicked="query.location = location.name; $router.push({name: '', query}); console.log($route.query)" class="text-left text-secondary hover:text-primary py-2 duration-500 ease-in-out" v-for="location in locations" :key="location.id">{{location.name}}</UiButtonsTertiary>
-          </div>
-
+      <template #accordion-content content_class="px-3 py-2">
+        <div :class="toggleSubcategories ? 'hidden' : 'block'" class="mt-3">
+          <ul v-if="categoriesLoaded" class="flex gap-2 flex-wrap text-left">
+            <li v-for="category in categories" :key="category.id">
+              <UiButtonsTertiary @clicked="chooseCategory(category)" class="text-left text-secondary hover:text-primary py-2 duration-500 ease-in-out">{{category.name}}</UiButtonsTertiary>
+            </li>
+          </ul>
           <div v-else class="flex items-center justify-center">
             <UiIconsLoading class="text-accent-100 h-10 w-10" />
           </div>
-        </template>
-      </AccordionItem>
+        </div>
+
+        <div :class="toggleSubcategories ? 'block' : 'hidden'" class="mt-3 px-5">
+          <UiButtonsSecondary @clicked="toggleSub()" class="flex gap-2 items-center mb-5">
+            <span><UiIconsArrow class="w-4" /></span>
+            {{ categoryName }}
+          </UiButtonsSecondary>
+          <ul v-if="subcategoryLoaded" class="flex gap-2 flex-wrap text-left">
+            <li v-for="item in subcategory" :key="subcategory.id">
+              <UiButtonsTertiary @clicked="chooseSubcategory()" class="text-left text-secondary hover:text-primary py-2 duration-500 ease-in-out">{{item.name}}</UiButtonsTertiary>
+            </li>
+          </ul>
+          <div v-else class="flex items-center justify-center">
+            <UiIconsLoading class="text-accent-100 h-10 w-10" />
+          </div>
+        </div>
+      </template>
+    </AccordionItem>
+
+    <AccordionItem container_class="border border-accent-100 p-2 px-3 rounded-md"
+      trigger_class="font-medium text-lg">
+      <template #accordion-trigger >
+        <h4>Locations</h4>
+      </template>
+
+      <template #accordion-content content_class="px-3 py-2">
+        <div v-if="locationsLoaded" class="mt-3 flex gap-2 flex-wrap text-left">
+          <UiButtonsTertiary @clicked="chooseLocation()" class="text-left text-secondary hover:text-primary py-2 duration-500 ease-in-out" v-for="location in locations" :key="location.id">{{location.name}}</UiButtonsTertiary>
+        </div>
+
+        <div v-else class="flex items-center justify-center">
+          <UiIconsLoading class="text-accent-100 h-10 w-10" />
+        </div>
+      </template>
+    </AccordionItem>
     </Accordion>
   </aside>
 </template>
-<script setup>
-import { onBeforeMount, ref } from "vue";
-import { useRouter } from 'vue-router'
+<script>
+import { mapActions, mapState } from 'pinia'
+import { useFilters } from "@/stores/filters";
 import { useQuery } from "@/composables/useQuery";
-const route = useRoute()
-const router = useRouter()
-const query = useQuery()
-const categoryLoaded = ref(false)
-const subcategoryLoaded = ref(false)
-const locationsLoaded = ref(false)
-const toggleSubcategories = ref(false)
-let locations = []
-let categories = []
-let categoryName = []
-let subcategories = []
-const getCategories = async () => {
-  const { pending, data: user_auth_data, error: user_auth_error } = await useCustomFetch('api/categories/', {
-    method: 'GET',
-    onResponse(res) {
-      if (res.response.status == 200) {
-        categories = res.response._data
-        categoryLoaded.value = true
+const { query } = useQuery()
+export default {
+  data(){
+    return {
+      categoriesLoaded : false,
+      subcategoryLoaded : false,
+      locationsLoaded : false,
+      toggleSubcategories : false,
+      categoryName : '',
+    }
+  },
+  methods: {
+    ...mapActions(useFilters, { 
+      getCategories: 'fetchCategories',
+      getLocations: 'fetchLocations',
+      selectCategory: 'selectCategory'
+    }),
+
+    toggleSub(){
+      this.toggleSubcategories = !this.toggleSubcategories
+    },
+
+    chooseCategory(category){
+      this.selectCategory(category);
+      this.categoryName = category.name
+      query.category = category.name;
+      this.$router.push({name: '', query})
+      if (this.subcategory != []) {
+        this.toggleSub()
+        this.subcategoryLoaded = true
       }
     },
-  })
-};
 
-const selectCategory = async (category) => {
-  const { pending, data, error } = await useCustomFetch(`api/categories/${category.id}/subcategories/`, {
-    method: 'GET',
-    onResponse(res) {
-      if (res.response.status == 200) {
-        categoryName = res.response._data.category.name
-        subcategories = res.response._data.subcategories
-        subcategoryLoaded.value = true
-      }
+    chooseSubcategory(){
+      query.subcategory = item.name; 
+      $router.push({name: 'marketplace-products', query});
     },
-  })
 
-  toggleSub()
+    chooseLocation(){
+      query.location = location.name;
+      $router.push({name: 'marketplace-products', query});
+    }
+  },
+  computed: {
+    ...mapState(useFilters, {
+      categories: 'getCategories',
+      locations: 'getLocations',
+      subcategory: 'getSubcategory'
+    })
+  },
+  created(){
+    this.getCategories();
+    this.categoriesLoaded = true;
+    this.getLocations();
+    this.locationsLoaded = true;
+  }
 }
-
-function toggleSub() {
-  toggleSubcategories.value = !toggleSubcategories.value
-}
-
-const getLocations = async () => {
-  const { pending, data: locationData, error: user_auth_error } = await useCustomFetch('api/locations/', {
-    method: 'GET',
-    onResponse(res) {
-      if (res.response.status == 200) {
-        locations = useState('locations', () => res.response._data).value
-        locationsLoaded.value = true
-      }
-    },
-  })
-};
-
-onBeforeMount(() => {
-  getCategories();
-  getLocations();
-})
 </script>
