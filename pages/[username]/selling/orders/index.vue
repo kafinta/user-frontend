50f6bc3ad46fb92d62291fbda408238d41ebaaba
Quadir <aquadirmuhammad@gmail.com>
2024-04-02 13:18:22 +0100

    chore: orders page design complete

--- pages/[username]/selling/orders/index.vue
+++ pages/[username]/selling/orders/index.vue
@@ -1,34 +1,32 @@
 <template>
   <LayoutsSellerDashboard page_title="Orders Overview">
-    <table class="w-full table-auto text-secondary">
-      <thead class="text-left bg-accent-100 border border-accent-100">
-        <tr>
-          <th class="text-sm md:text-base 2xl:text-lg font-medium">Product</th>
-          <th class="text-sm md:text-base 2xl:text-lg font-medium">Order No</th>
-          <th class="text-sm md:text-base 2xl:text-lg font-medium">Quantity</th>
-          <th class="text-sm md:text-base 2xl:text-lg font-medium">Amount</th>
-          <th class="text-sm md:text-base 2xl:text-lg font-medium">Date</th>
-          <th class="text-sm md:text-base 2xl:text-lg font-medium">Status</th>
-        </tr>
-      </thead>
-
-      <tbody class="gap-6">
-        <tr @click="" class="text-left border border-accent-100 rounded-2xl cursor-pointer" v-for="items in 5">
-          <td>
-            <!-- <ProductsImage :imagePath="image" class="w-24 rounded-sm"></ProductsImage> -->
-            <div class="product w-24 h-auto rounded-md bg-accent-100"></div>
-          </td>
-          <td class="text-sm md:text-base 2xl:text-lg">5</td>
-          <td class="text-sm md:text-base 2xl:text-lg">0000</td>
-          <td class="text-sm md:text-base 2xl:text-lg">{{ new Date().toLocaleDateString() }}</td>
-          <td><ProductsPrice class="w-1/4" /></td>
-          <td class="text-sm md:text-base 2xl:text-lg">
-            <button :class="status === 'completed' ? 'text-green-600' : status === 'cancelled' ? 'text-red-600' : status === 'shipped' ? 'text-orange-500' : status === 'delivered' ? 'text-green-600' : status === 'pending' ? 'text-pink-500' : 'text-secondary'" class="capitalize w-1/4">{{ status }}</button>
-          </td>
-        </tr>
-      </tbody>
-
-    </table>
+    <ul class="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-5 w-full" v-for="cards in 6">
+      <li class="rounded-2xl border border-accent-100 block">
+        <ProductsImage class="rounded-t-2xl" />
+        <div class="p-5 flex flex-col gap-5">
+          <ProductsName>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</ProductsName>
+          <ul class="grid grid-cols-2 gap-5">
+            <li>
+              <UiTypographyH3>5</UiTypographyH3>
+              <UiTypographyP>Quantity</UiTypographyP>
+            </li>
+            <li>
+              <UiTypographyH3>$9999</UiTypographyH3>
+              <UiTypographyP>Price</UiTypographyP>
+            </li>
+            <li>
+              <UiTypographyH3>{{ new Date().toLocaleDateString() }}</UiTypographyH3>
+              <UiTypographyP>Date</UiTypographyP>
+            </li>
+            <li>
+              <h3 class="text-xl md:text-2xl 2xl:text-3xl font-medium" :class="status === 'completed' ? 'text-green-600' : status === 'cancelled' ? 'text-red-600' : status === 'shipped' ? 'text-orange-500' : status === 'delivered' ? 'text-green-600' : status === 'pending' ? 'text-pink-500' : 'text-secondary'">Pending</h3>
+              <UiTypographyP>Status</UiTypographyP>
+            </li>
+          </ul>
+          <UiButtonsPrimary :flexdisplay="true">View</UiButtonsPrimary>
+        </div>
+      </li>
+    </ul>
   </LayoutsSellerDashboard>
 </template>
 <script>
