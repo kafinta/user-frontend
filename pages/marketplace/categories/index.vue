b569509e6699f2748cb21fa86101beb80aac2782
Quadir <aquadirmuhammad@gmail.com>
2024-05-16 16:16:49 +0100

    implemented env variable

--- pages/marketplace/categories/index.vue
+++ pages/marketplace/categories/index.vue
@@ -3,7 +3,7 @@
     <Container>
       <ul class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
         <li v-if="categoriesLoaded" v-for="category in categories" :key="category.id">
-          <UiCards @clicked="chooseCategory(category)" :title="category.name" :backgroundImagePath="'http://localhost:8000' + category.image" class="w-full"/>
+          <UiCards @clicked="chooseCategory(category)" :title="category.name" :backgroundImagePath="config.public.base_url + category.image" class="w-full"/>
         </li>
         <li v-else class="flex items-center justify-center">
           <UiIconsLoading class="text-primary h-10 w-10" />
