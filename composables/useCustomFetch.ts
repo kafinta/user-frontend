f16f500964cb94001a3576c830c18943642a4ad7
Quadir <aquadirmuhammad@gmail.com>
2024-09-12 11:51:26 +0100

    api composable updated

--- composables/useCustomFetch.ts
+++ composables/useCustomFetch.ts
@@ -1,39 +1,25 @@
-import type { UseFetchOptions } from 'nuxt/app'
+import type { UseFetchOptions } from '#app'
+import type { Ref } from 'vue'
 import { defu } from 'defu'
 
-export function useCustomFetch<T>(url: string, options: UseFetchOptions<T> = {}) {
-    const XSRF_TOKEN = useCookie('XSRF-TOKEN')
-    const config = useRuntimeConfig()
+export function useCustomFetch<T>(
+  url: string | Ref<string>,
+  options: UseFetchOptions<T> = {}
+) {
+  const XSRF_TOKEN = useCookie('XSRF-TOKEN')
+  const config = useRuntimeConfig()
 
-    const defaults: UseFetchOptions<T> = {
-        baseURL: config.public.base_url as string,
-        // cache request
-        key: url,
-        credentials: 'include',
+  const defaults: UseFetchOptions<T> = {
+    baseURL: config.public.base_url,
+    credentials: 'include',
+    headers: {
+      'accept': 'application/json, text/plain, */*',
+      'X-XSRF-TOKEN': XSRF_TOKEN.value ?? '',
+    },
+  }
 
-        // set user token if connected
-        headers:
-        {
-            'accept': 'application/json, text/plain, */*',
-            'X-XSRF-TOKEN': XSRF_TOKEN.value ? XSRF_TOKEN.value : '',
-        },
+  // Merge defaults with user provided options
+  const params = defu(options, defaults)
 
-        onResponse(_ctx) {
-            // _ctx.response._data = new myBusinessResponse(_ctx.response._data)
-        //    toast.success(_ctx.response._data.message)
-        },
-
-        onResponseError(_ctx) {
-            if (_ctx.response.status == 401) {
-                const user_session = useCookie('user_session', { sameSite: 'lax' })
-                
-            } else {
-            }
-        }
-    }
-
-    // for nice deep defaults, please use unjs/defu
-    const params = defu(options, defaults)
-
-    return useFetch(url, params)
+  return useFetch<T>(url, params as any)
 }
