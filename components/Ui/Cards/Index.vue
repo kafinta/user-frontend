04104258a74047358fe4c65641320f4e9ae68b14
Quadir <aquadirmuhammad@gmail.com>
2025-03-26 10:21:40 +0100

    update: nuxt image implemented

--- components/Ui/Cards/Index.vue
+++ components/Ui/Cards/Index.vue
@@ -1,24 +1,38 @@
 <template>
   <button @click="$emit('clicked')" class="cursor-pointer group rounded-xl border border-accent-300 p-3 h-full bg-secondary">
-    <UiTypographyH3 color="white" class="text-left  mt-2 mb-5">{{title}}</UiTypographyH3>
-    <img :src="backgroundImagePath" class="aspect-[4/3] w-full rounded-md" alt="Image">
+    <UiTypographyH3 color="white" class="text-left h-[2lh] mb-5">{{ title }}</UiTypographyH3>
+    <NuxtImg 
+      :src="imageSrc" 
+      class="aspect-[4/3] w-full rounded-md" 
+      :alt="alt" 
+    />
   </button>
 </template>
 
-<script>
-export default {
-  props: {
-    backgroundImagePath: String,
-    title: String,
-    query: Object
-  },
-}
-</script>
+<script setup>
+const config = useRuntimeConfig();
 
-<style>
-  .background {
-    background-position: center center !important;
-    background-size: cover !important;
-    background-repeat: no-repeat !important;
+const props = defineProps({
+  image_path: {
+    type: String,
+    default: ''
+  },
+  title: {
+    type: String,
+    default: ''
+  },
+  alt: {
+    type: String,
+    default: ''
   }
-</style>
+})
+
+const imageSrc = computed(() => {
+  return props.image_path.replace('http://localhost/', config.public.base_url)
+})
+
+onMounted(()=> {
+  console.log('base url' + config.public.base_url);
+  
+})
+</script>
