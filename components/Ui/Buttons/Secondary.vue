14b78f836550ba99796131006b82d511baee06d5
Quadir <aquadirmuhammad@gmail.com>
2025-03-26 17:07:31 +0100

    refactor: buttons updated to include nuxtlink

--- components/Ui/Buttons/Secondary.vue
+++ components/Ui/Buttons/Secondary.vue
@@ -1,5 +1,16 @@
 <template>
+  <NuxtLink :to="url" class="font-medium text-sm xl:text-base 2xl:text-lg text-secondary hover:text-primary ease-in-out duration-500 border-b border-transparent hover:border-primary select-none outline-none">
+    <slot />
+  </NuxtLink>
   <button @click="$emit('clicked')" class="font-medium text-sm xl:text-base 2xl:text-lg text-secondary hover:text-primary ease-in-out duration-500 border-b border-transparent hover:border-primary select-none outline-none">
     <slot />
   </button>
 </template>
+<script setup>
+const props = defineProps({
+  url: {
+    type: Object,
+    default: null
+  }
+})
+</script>
--- components/Ui/Buttons/Tertiary.vue
+++ components/Ui/Buttons/Tertiary.vue
@@ -1,5 +1,11 @@
 <template>
-  <button 
+  <NuxtLink v-if="url"
+    :to="url"
+    class="py-2 px-5 text-secondary font-medium text-sm lg:text-base 2xl:text-lg justify-center duration-500 ease-in-out rounded-md active:text-secondary border hover:text-primary hover:border-primary focus:border-primary focus:text-primary outline-none" 
+    :class="[flexdisplay ? 'flex w-full text-center gap-3 items-center' : 'w-fit text-left']">
+    <slot />
+  </NuxtLink>
+  <button v-else
     @click="$emit('clicked')"
     class="py-2 px-5 text-secondary font-medium text-sm lg:text-base 2xl:text-lg justify-center duration-500 ease-in-out rounded-md active:text-secondary border hover:text-primary hover:border-primary focus:border-primary focus:text-primary outline-none" 
     :class="[flexdisplay ? 'flex w-full text-center gap-3 items-center' : 'w-fit text-left']">
@@ -9,12 +15,15 @@
 
 <script>
 export default {
-  name: 'UiIconsLoadingButton',
   props: {
     flexdisplay: {
       default: false,
       type: Boolean
     },
+    url: {
+      type: Object,
+      default: null
+    }
   },
 }
 </script>
