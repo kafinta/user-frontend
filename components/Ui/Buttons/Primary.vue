76f84878013466d831c73d50952abf7fa80fec36
Quadir <aquadirmuhammad@gmail.com>
2025-03-29 17:38:26 +0100

    refactor: component rendering logic updated

--- components/Ui/Buttons/Primary.vue
+++ components/Ui/Buttons/Primary.vue
@@ -1,10 +1,10 @@
 <template>
-  <NuxtLink :to="url"
+  <NuxtLink :to="url" v-if="url"
     class="py-2 px-5 text-white font-medium text-base 2xl:text-lg justify-center duration-500 ease-in-out rounded-md active:bg-primary outline-none" 
     :disabled="loading || disabled" :class="[flexdisplay ? 'flex w-full items-center gap-3' : 'block w-fit', standout ? 'bg-primary hover:bg-opacity-80 focus:bg-opacity-80': 'bg-secondary hover:bg-primary focus:bg-primary', disabled ? 'bg-accent-300 text-accent-100 hover:bg-accent-300 hover:text-accent-100': '']">
       <slot />
   </NuxtLink>
-  <button 
+  <button v-else
     @click="$emit('clicked')"
     class="py-2 px-5 text-white font-medium text-base 2xl:text-lg justify-center duration-500 ease-in-out rounded-md active:bg-primary outline-none" 
     :disabled="loading || disabled" :class="[flexdisplay ? 'flex w-full items-center gap-3' : 'block w-fit', standout ? 'bg-primary hover:bg-opacity-80 focus:bg-opacity-80': 'bg-secondary hover:bg-primary focus:bg-primary', disabled ? 'bg-accent-300 text-accent-100 hover:bg-accent-300 hover:text-accent-100': '']">
