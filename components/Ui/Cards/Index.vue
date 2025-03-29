1d47ac8f7719280b2630904ed954fee477682876
Quadir <aquadirmuhammad@gmail.com>
2025-03-29 18:19:33 +0100

    refactor: image backup added

--- components/Ui/Cards/Index.vue
+++ components/Ui/Cards/Index.vue
@@ -1,15 +1,26 @@
 <template>
   <button @click="$emit('clicked')" class="cursor-pointer group rounded-xl p-3 h-full bg-secondary w-full">
     <UiTypographyH3 color="white" class="text-left h-[2lh] mb-5">{{ title }}</UiTypographyH3>
-    <NuxtImg 
-      :src="imageSrc" 
-      class="aspect-[4/3] w-full rounded-md" 
-      :alt="alt" 
-    />
+    <template v-if="imageLoaded !== false">
+      <NuxtImg 
+        :src="imageSrc" 
+        class="aspect-[4/3] w-full rounded-md" 
+        :alt="alt"
+        @error="imageLoaded = false"
+        @load="imageLoaded = true"
+      />
+  </template>
+  <div 
+    v-else
+    class="aspect-[4/3] w-full rounded-md bg-gray-300 flex items-center justify-center"
+  >
+    <span class="text-gray-600">{{ alt || 'Image not available' }}</span>
+  </div>
   </button>
 </template>
 
 <script setup>
+import { ref } from 'vue'
 const config = useRuntimeConfig();
 
 const props = defineProps({
@@ -30,4 +41,5 @@ const props = defineProps({
 const imageSrc = computed(() => {
   return props.image_path.replace('http://localhost/', config.public.base_url)
 })
+const imageLoaded = ref(null)
 </script>
