02667f08e8843c9cba53a77fdd2f9f63fdb5e93c
Quadir <aquadirmuhammad@gmail.com>
2025-03-29 17:37:35 +0100

    update: url prop on button components implemented

--- components/Navigation/Footer.vue
+++ components/Navigation/Footer.vue
@@ -1,37 +1,37 @@
 <template>
   <footer class="border-t border-accent-200 w-full">
     <Container class="grid grid-cols-2 lg:grid-cols-4 gap-5 max-w-7xl mx-auto py-8">
-      <div class="flex flex-col gap-2 text-left">
+      <div class="flex flex-col gap-2 text-left items-start">
         <h4 class="uppercase text-secondary text-base font-medium mb-2">Company</h4>
-        <NuxtLink to=""><UiButtonsSecondary>About</UiButtonsSecondary></NuxtLink>
-        <NuxtLink to=""><UiButtonsSecondary>Privacy Policy</UiButtonsSecondary></NuxtLink>
-        <NuxtLink to=""><UiButtonsSecondary>Terms of Service</UiButtonsSecondary></NuxtLink>
-        <NuxtLink to=""><UiButtonsSecondary>Copyright & Trademark</UiButtonsSecondary></NuxtLink>
-        <NuxtLink to=""><UiButtonsSecondary class="text-left w-fit">Intellectual Property Claims</UiButtonsSecondary></NuxtLink>
+        <UiButtonsSecondary :url="{}">About</UiButtonsSecondary>
+        <UiButtonsSecondary :url="{}">Privacy Policy</UiButtonsSecondary>
+        <UiButtonsSecondary :url="{}">Terms of Service</UiButtonsSecondary>
+        <UiButtonsSecondary :url="{}">Copyright & Trademark</UiButtonsSecondary>
+        <UiButtonsSecondary :url="{}" class="text-left w-fit">Intellectual Property Claims</UiButtonsSecondary>
       </div>
 
-      <div class="flex flex-col gap-2">
+      <div class="flex flex-col gap-2 items-start">
         <h4 class="uppercase text-secondary text-base font-medium mb-2">Services</h4>
-        <NuxtLink to=""><UiButtonsSecondary>Shop Products</UiButtonsSecondary></NuxtLink>
-        <NuxtLink to=""><UiButtonsSecondary>Products Pricing</UiButtonsSecondary></NuxtLink>
-        <NuxtLink to=""><UiButtonsSecondary>Hire Artisans</UiButtonsSecondary></NuxtLink>
-        <NuxtLink to=""><UiButtonsSecondary>Become a Seller</UiButtonsSecondary></NuxtLink>
-        <NuxtLink to=""><UiButtonsSecondary>Join as an Artisan</UiButtonsSecondary></NuxtLink>
+        <UiButtonsSecondary :url="{}">Shop Products</UiButtonsSecondary>
+        <UiButtonsSecondary :url="{}">Products Pricing</UiButtonsSecondary>
+        <UiButtonsSecondary :url="{}">Hire Artisans</UiButtonsSecondary>
+        <UiButtonsSecondary :url="{}">Become a Seller</UiButtonsSecondary>
+        <UiButtonsSecondary :url="{}">Join as an Artisan</UiButtonsSecondary>
       </div>
 
-      <div class="flex flex-col gap-2">
+      <div class="flex flex-col gap-2 items-start">
         <h4 class="uppercase text-secondary text-base font-medium mb-2">Support</h4>
-        <NuxtLink to=""><UiButtonsSecondary>Help & Support</UiButtonsSecondary></NuxtLink>
-        <NuxtLink to=""><UiButtonsSecondary>Trust & Safety</UiButtonsSecondary></NuxtLink>
-        <NuxtLink to=""><UiButtonsSecondary>Your Orders</UiButtonsSecondary></NuxtLink>
-        <NuxtLink to=""><UiButtonsSecondary>Shipping & Delivery</UiButtonsSecondary></NuxtLink>
-        <NuxtLink to=""><UiButtonsSecondary>Return Policy</UiButtonsSecondary></NuxtLink>
+        <UiButtonsSecondary :url="{}">Help & Support</UiButtonsSecondary>
+        <UiButtonsSecondary :url="{}">Trust & Safety</UiButtonsSecondary>
+        <UiButtonsSecondary :url="{}">Your Orders</UiButtonsSecondary>
+        <UiButtonsSecondary :url="{}">Shipping & Delivery</UiButtonsSecondary>
+        <UiButtonsSecondary :url="{}">Return Policy</UiButtonsSecondary>
       </div>
 
       <div class="flex flex-col gap-2">
-        <h4 class="uppercase text-secondary text-base font-medium mb-2">Download Apps</h4>
-        <NuxtLink to=""><img src="/images/icons/apple.svg" class="w-36" alt=""></NuxtLink>
-        <NuxtLink to=""><img src="/images/icons/google.svg" class="w-36" alt=""></NuxtLink>
+        <h4 class="uppercase text-secondary text-base font-medium mb-2 items-start">Download Apps</h4>
+        <img src="/images/icons/apple.svg" class="w-36" alt="">
+        <img src="/images/icons/google.svg" class="w-36" alt="">
       </div>
     </Container>
 
@@ -42,23 +42,23 @@
       </div>
 
       <div class="flex items-center gap-5">
-        <NuxtLink to="">
+        
           <div class="duration-500 ease-in-out cursor-pointer h-8 w-8 bg-accent-100 hover:bg-accent-200 rounded-md flex justify-center items-center">
             <img src="/images/logos/facebook_f.svg" class="w-6 h-6" alt="">
           </div>
-        </NuxtLink>
+        
 
-        <NuxtLink to="">
+        
           <div class="duration-500 ease-in-out cursor-pointer h-8 w-8 bg-accent-100 hover:bg-accent-200 rounded-md flex justify-center items-center">
             <img src="/images/logos/twitter.svg" class="w-6 h-6" alt="">
           </div>
-        </NuxtLink>
+        
 
-        <NuxtLink to="">
+        
           <div class="duration-500 ease-in-out cursor-pointer h-8 w-8 bg-accent-100 hover:bg-accent-200 rounded-md flex justify-center items-center">
             <img src="/images/logos/instagram.svg" class="w-6 h-6" alt="">
           </div>
-        </NuxtLink>
+        
       </div>
     </Container>
   </footer>
--- components/Navigation/NavBar/Index.vue
+++ components/Navigation/NavBar/Index.vue
@@ -15,19 +15,19 @@
       <nav :class="menu_revealed ? 'translate-x-0' : '-translate-x-full'" class="md:h-fit z-100 md:static w-full md:w-fit fixed h-screen left-0 top-[66px] md:top duration-150 ease-in-out md:translate-x-0 bg-white">
         <ul class="grid grid-cols-1 place-items-center md:flex gap-5 md:gap-3 lg:gap-5 items-center px-6 pt-20 md:p-0">
           <li>
-            <UiButtonsSecondary  @clicked="toggleSearchBox" class="flex gap-2 group items-center">
+            <UiButtonsSecondary @clicked="toggleSearchBox" class="flex gap-2 group items-center">
               <UiIconsSearch :class="keep_button_hovered ? 'text-primary' : 'text-secondary'" class=" group-hover:text-primary w-5 h-5 duration-500 ease-in-out"></UiIconsSearch>
               <span :class="keep_button_hovered ? 'text-primary border-primary' : ''">Search</span>
             </UiButtonsSecondary>
           </li>
           <li>
-            <UiButtonsSecondary @clicked="$router.push({path: '/marketplace/'})">Marketplace</UiButtonsSecondary>
+            <UiButtonsSecondary :url="{path: '/marketplace/'}">Marketplace</UiButtonsSecondary>
           </li>
           <li>
-            <UiButtonsSecondary @clicked="$router.push({name: 'auth-login'})">Sign In</UiButtonsSecondary>
+            <UiButtonsSecondary :url="{name: 'auth-login'}">Sign In</UiButtonsSecondary>
           </li>
           <li >
-            <UiButtonsPrimary @clicked="$router.push({name: 'auth-signup'})">Sign Up</UiButtonsPrimary>
+            <UiButtonsPrimary :url="{name: 'auth-signup'}">Sign Up</UiButtonsPrimary>
           </li>
         </ul>
       </nav>
