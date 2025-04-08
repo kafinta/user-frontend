707527e77f286eb94f21cb25332084f30f56fc40
Quadir <aquadirmuhammad@gmail.com>
2025-04-08 21:24:46 +0100

    refactor: auth store and primevue implemented

--- pages/auth/login.vue
+++ pages/auth/login.vue
@@ -1,48 +1,5 @@
-<script setup>
-import { toast } from 'vue3-toastify';
-import 'vue3-toastify/dist/index.css';
-import { ref } from "vue";
-import { useRouter } from 'vue-router'
-
-const router = useRouter()
-
-let is_small = ref(false);
-let loadingState = ref(false);
-let error_state = ref(false);
-const email = ref();
-const password = ref();
-
-const signIn= async () => {
-  loadingState.value = true;
-  const { data: csrf_token_data, error: csrf_token_error } = await useCustomFetch('/sanctum/csrf-cookie')
-  const { pending, data: user_auth_data, error: user_auth_error } = await useCustomFetch('/user/auth/login', {
-    method: 'POST',
-    body: {
-        email: email.value,
-        password: password.value
-    },
-    onResponse(res) {
-      if (res.response.status == 200) {
-        updateUser(res.response._data.data)
-        toast.success(res.response._data.message, {
-          theme: 'colored',
-          position: toast.POSITION.BOTTOM_RIGHT,
-        })
-        router.push({name: 'username-buying-dashboard', params: {username: user.value.username}})
-      } else {
-      toast.error(res.response._data.message, {
-        position: toast.POSITION.BOTTOM_RIGHT,
-        theme: 'colored'
-      })
-    }
-      email.value = '',
-      password.value = ''
-      loadingState.value = false
-    },
-  })
-}
-</script>
 <template>
+  <Toast position="top-right" />
   <div class="flex select-none">
     <div class="background hidden lg:flex w-2/3 bg-cover bg-center py-5 px-10 relative flex-col justify-end">
       <div class="bg-white rounded-md bg-opacity-80 p-5 w-full">
@@ -56,19 +13,20 @@ const signIn= async () => {
 
     <div class="w-full lg:w-1/2 h-screen flex items-center justify-center mx-auto px-6 md:px-8 lg:px-10 relative">
       <NavigationLogo @click="returnHome()" class="absolute top-5 left-10 w-36 lg:hidden" :class="is_small ? 'hidden' : ''" />
-      <div @submit.prevent="signIn()" action="" class="w-full md:w-2/3 lg:w-full rounded-xl p-5" :class="is_small ? 'p-0' : ''">
+      <div @submit.prevent="handleSignin()" action="" class="w-full md:w-2/3 lg:w-full rounded-xl p-5" :class="is_small ? 'p-0' : ''">
         <h1 :class="is_small ? 'text-2xl' : ''" class="font-medium text-3xl w-fit text-secondary">Welcome back.</h1>
         <p :class="is_small ? 'mb-4' : ''" class="text-sm text-secondary mb-8">Enter your details to resume your session.</p>
 
         <form class="grid gap-4">
-          <FormInput label="Email" v-model:inputValue="email" placeholder="Enter your email address"></FormInput>
-          <div class="w-full mb-5">
-            <FormInput :error="error_state" label="Password" type="password" v-model:inputValue="password" placeholder="Enter your password"></FormInput>
-            <div class="flex justify-between items-center mt-2 w-full">
-              <p :class="error_state ? 'opacity-100' : 'opacity-0'" class="text-sm text-red-600 mt-2 duration-300 ease-in-out ">Wrong username or password</p>
-              <nuxt-link to="/auth/forgot" :class="error_state ? 'hidden' : 'block'" class="text-sm text-secondary text-opacity-50 hover:text-opacity-100 duration-500 ease-in-out">Forgot password?</nuxt-link>
-            </div>
-          </div>
+          <FloatLabel variant="on" class="w-full">
+            <InputText id="email_label" type="email" v-model="email" fluid/>
+            <label for="email_label">Email</label>
+          </FloatLabel>
+          <FloatLabel variant="on">
+            <InputText id="password_label" type="password" v-model="password" fluid />
+            <label for="password_label">Password</label>
+          </FloatLabel>
+          <nuxt-link to="/auth/forgot" :class="error_state ? 'hidden' : 'block'" class="mb-5 text-sm text-secondary text-opacity-50 hover:text-opacity-100 duration-500 ease-in-out">Forgot password?</nuxt-link>
 
           <FormButton :loading="loadingState">Sign In</FormButton>
         </form>
@@ -79,6 +37,58 @@ const signIn= async () => {
   </div>
 
 </template>
+<script setup>
+import { ref } from "vue";
+import { storeToRefs } from 'pinia';
+import { useAuthStore } from '~/stores/auth';
+import { useToast } from "primevue/usetoast";
+import { useRouter } from 'vue-router';
+
+const toast = useToast();
+const authStore = useAuthStore();
+const { message, isLoading, status } = storeToRefs(authStore);
+const router = useRouter();
+
+let is_small = ref(false);
+let loadingState = ref(false);
+let error_state = ref(false);
+const email = ref();
+const password = ref();
+
+async function handleSignin() {
+  try {
+    authStore.clearMessages();
+    await authStore.login({
+      email: email.value,
+      password: password.value,
+    });
+    console.log(status.value)
+    if(status.value === 'success') {
+      toast.add({
+        severity: 'success',
+        summary: 'Success',
+        detail: message.value, // Fixed: Need to use .value for refs
+        life: 3000,
+      });
+      router.push({name: 'auth-verify'});
+    } else if (status.value === 'error') { // Fixed: Using status.value
+      toast.add({
+        severity: 'error',
+        summary: 'Error',
+        detail: message.value, // Fixed: Need to use .value for refs
+        life: 3000,
+      });
+    }
+  } catch (error) {
+    toast.add({
+      severity: 'error',
+      summary: 'Error',
+      detail: 'An unexpected error occurred',
+      life: 3000,
+    });
+  }
+}
+</script>
 <style>
 .background {
   background: url('/images/login.jpg');
