0b21bb46717fb83cfd36c6f7418e6f327fe81cc0
Quadir <aquadirmuhammad@gmail.com>
2025-05-07 21:18:12 +0100

    refactor: enhance PrimeVue configuration for improved theme management

--- plugins/primevue.ts
+++ plugins/primevue.ts
@@ -1,10 +1,10 @@
 import { defineNuxtPlugin } from "#app";
 import PrimeVue from "primevue/config";
 import ToastService from 'primevue/toastservice';
-import Lara from '@primeuix/themes/lara';
+import Aura from '@primeuix/themes/aura';
 import { definePreset } from '@primeuix/themes';
 
-const MyPreset = definePreset(Lara, {
+const MyPreset = definePreset(Aura, {
   semantic: {
       primary: {
         50: '#f9f6eb',
@@ -36,7 +36,6 @@ const MyPreset = definePreset(Lara, {
   }
 });
 
-// import "primeicons/primeicons.css";
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.use(PrimeVue, {
     ripple: true,
