96da7b9a4959f5d2a29ccb988825c4e5cecfba6c
Quadir <aquadirmuhammad@gmail.com>
2025-05-07 22:08:09 +0100

    refactor: simplify signup page layout and improve structure

--- pages/auth/signup.vue
+++ pages/auth/signup.vue
@@ -1,62 +1,50 @@
 <template>
-  <div class="flex flex-row-reverse select-none">
-    <div class="background hidden lg:flex w-2/3 bg-cover bg-center py-5 px-10 relative flex-col justify-end">
+  <div class="flex justify-center items-center h-screen">
+    <main class="w-full max-w-md mx-auto rounded-xl p-5 border-accent-200 border space-y-8">
+      <NavigationLogo @click="returnHome()" class="w-48 mx-auto" />
 
-      <div class="bg-white rounded-md bg-opacity-80 p-5 w-full">
-        <h1 class="text-2xl text-secondary">With the top of the line products and services available, my home has never looked or felt better. Amazing platform, definitely recommend!</h1>
-
-        <p class="text-base 2xl:text-lg text-primary mt-5">Olivia Rhye</p>
-        <p class="text-sm text-secondary 2xl:text-base">Founder, Catalog</p>
-
+      <div>
+        <UiTypographyH2 class="font-medium text-3xl  text-secondary text-center">Create account.</UiTypographyH2>
+        <UiTypographyP class="text-sm text-secondary text-center">You are just a few steps away.</UiTypographyP>
       </div>
-    </div>
 
-    <div class="w-full lg:w-1/2 h-screen flex items-center justify-center mx-auto px-6 md:px-8 lg:px-10 relative">
-      <NavigationLogo @click="returnHome()" class="absolute top-5 left-10 w-36 lg:hidden" :class="is_small ? 'hidden' : ''" />
+      <form @submit.prevent="handleSignup()" action="" class="grid gap-4">
+        <FloatLabel variant="on" class="w-full">
+          <InputText id="email_label" type="email" v-model="email" fluid/>
+          <label for="email_label">Email</label>
+        </FloatLabel>
+        <FloatLabel variant="on">
+          <InputText id="username_label" type="text" v-model="username" fluid />
+          <label for="username_label">Username</label>
+        </FloatLabel>
+        <FloatLabel variant="on">
+          <InputText id="password_label" type="password" v-model="password" fluid />
+          <label for="password_label">Password</label>
+        </FloatLabel>
 
-      <main class="w-full md:w-2/3 lg:w-full rounded-xl p-5" :class="is_small ? 'p-0' : ''">
-        <h1 :class="is_small ? 'text-2xl' : ''" class="font-medium text-3xl w-fit text-secondary">Create account.</h1>
-        <p :class="is_small ? 'mb-4' : ''" class="text-sm text-secondary mb-8">You are just a few steps away.</p>
-
-        
-        <form @submit.prevent="handleSignup()" action="" class="grid gap-4">
-          <FloatLabel variant="on" class="w-full">
-            <InputText id="email_label" type="email" v-model="email" fluid/>
-            <label for="email_label">Email</label>
-          </FloatLabel>
-          <FloatLabel variant="on">
-            <InputText id="username_label" type="text" v-model="username" fluid />
-            <label for="username_label">Username</label>
-          </FloatLabel>
-          <FloatLabel variant="on">
-            <InputText id="password_label" type="password" v-model="password" fluid />
-            <label for="password_label">Password</label>
-          </FloatLabel>
-
-          <FormButton :loading="isLoading" class="">Sign Up</FormButton>
-        </form>
+        <FormButton :loading="isLoading" class="">Sign Up</FormButton>
         <p class="text-sm w-fit mx-auto mt-2 text-secondary text-center">Already a member? <NuxtLink to="/auth/login" class="duration-500 ease-in-out hover:text-primary">Sign In</NuxtLink></p>
-      </main>
-    </div>
-  </div>
+      </form>
 
+    </main>
+  </div>
 </template>
 <script setup>
 definePageMeta({
   middleware: ['auth'],
   authOnly: true
 })
+import { useRouter } from 'vue-router';
 import { ref } from "vue";
 import { storeToRefs } from 'pinia';
 import { useAuthStore } from '~/stores/auth';
-import { useRouter } from 'vue-router';
+import { useToast } from "primevue/usetoast";
 
-const toast = useToast();
 const authStore = useAuthStore();
 const { message, isLoading, status } = storeToRefs(authStore);
 const router = useRouter();
+const toast = useToast();
 
-let is_small = ref(false);
 const email = ref('');
 const username = ref('');
 const password = ref('');
@@ -73,7 +61,6 @@ async function handleSignup() {
       password: password.value,
       username: username.value
     });
-    console.log(status.value)
     if(status.value === 'success') {
       toast.add({
         severity: 'success',
@@ -100,8 +87,3 @@ async function handleSignup() {
   }
 }
 </script>
-<style>
-.background {
-  background-image: url('/images/register.jpg');
-}
-</style>
