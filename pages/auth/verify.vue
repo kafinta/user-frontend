357350076d268f6fdbfb572d15df030256c68764
Quadir <aquadirmuhammad@gmail.com>
2025-05-08 20:33:42 +0100

    refactor: reduce gap in verification form for improved layout

--- pages/auth/verify.vue
+++ pages/auth/verify.vue
@@ -7,7 +7,7 @@
         <UiTypographyH2 class="font-medium text-3xl  text-secondary text-center">Verify your email.</UiTypographyH2>
         <UiTypographyP class="text-sm text-secondary text-center">We sent a six digit code to your email. Enter it below to verify your account</UiTypographyP>
       </div>
-      <form @submit.prevent="handleVerification()" class="grid gap-6">
+      <form @submit.prevent="handleVerification()" class="grid gap-4">
         <InputOtp v-model="code" :length="6" integerOnly class="justify-between"/>
         <FormButton :loading="loadingState">Verify</FormButton>
       </form>
