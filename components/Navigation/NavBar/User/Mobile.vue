1d9016328efb55d15a885879bd7f07cdfefa04fa
Quadir <aquadirmuhammad@gmail.com>
2025-05-10 14:34:15 +0100

    refactor: update user menu components to use UiButtonsTertiary for improved consistency and functionality

--- components/Navigation/NavBar/User/Mobile.vue
+++ components/Navigation/NavBar/User/Mobile.vue
@@ -1,57 +1,94 @@
 <template>
   <div :class="menu_revealed ? 'translate-x-0' : '-translate-x-full'" class="z-100 w-full overflow-y-scroll fixed h-screen left-0 top-[67px] duration-150 ease-in-out bg-white md:hidden px-6 py-5">
     <div class="py-3">
+      <!-- Unauthenticated User Menu -->
       <ul v-if="!signedIn" class="flex flex-col gap-3">
-        <li>
-          <UiButtonsSecondary>Sign In</UiButtonsSecondary>
+        <li v-if="showMarketplaceLink">
+          <UiButtonsSecondary :url="{path: '/marketplace/'}">Marketplace</UiButtonsSecondary>
         </li>
         <li>
-          <UiButtonsPrimary>Sign Up</UiButtonsPrimary>
+          <UiButtonsSecondary :url="{name: 'auth-login'}">Sign In</UiButtonsSecondary>
+        </li>
+        <li>
+          <UiButtonsPrimary :url="{name: 'auth-signup'}">Sign Up</UiButtonsPrimary>
         </li>
       </ul>
 
+      <!-- Authenticated User Menu -->
       <ul v-if="signedIn" class="flex flex-col gap-3">
-        <li v-if="isSeller">
-          <UiButtonsTab @clicked="$router.push({name: 'username-selling', params: {username: 'username'}})" class="w-full text-left">Switch to Selling</UiButtonsTab>
+        <!-- Role switching -->
+        <li v-if="isSeller && showRoleSwitch">
+          <UiButtonsTertiary @clicked="$router.push({name: 'username-selling-dashboard', params: {username}})" class="w-full text-left">Switch to Selling</UiButtonsTertiary>
         </li>
-        <li>
-          <UiButtonsTab class="flex gap-3 items-center justify-start w-full" :flexdisplay="true" @clicked="$emit('cartClicked')">
+        <li v-if="isCustomer && showRoleSwitch">
+          <UiButtonsTertiary @clicked="$router.push({name: 'username-buying-dashboard', params: {username}})" class="w-full text-left">Switch to Buying</UiButtonsTertiary>
+        </li>
+
+        <!-- Optional features -->
+        <li v-if="showCart">
+          <UiButtonsTertiary class="flex gap-3 items-center justify-start w-full" :flexdisplay="true" @clicked="$emit('cartClicked')">
             Cart
-          </UiButtonsTab>
+          </UiButtonsTertiary>
+        </li>
+
+        <!-- Common dashboard items -->
+        <li>
+          <UiButtonsTertiary :to="{name: 'username-buying-dashboard', params: {username}}" class="w-full text-left">Dashboard</UiButtonsTertiary>
         </li>
         <li>
-          <UiButtonsTab :to="{name: 'username-buying-dashboard', params: {username: username}}" class="w-full text-left">Dashboard</UiButtonsTab>
-        </li>
-        <li>                  
-          <UiButtonsTab :to="{name: 'username-profile', params: {username: username}}" class="w-full text-left">Profile</UiButtonsTab>
-        </li>
-        <li>                  
-          <UiButtonsTab :to="{name: 'username-buying-dashboard', params: {username: username}}" class="w-full text-left">Orders</UiButtonsTab>
+          <UiButtonsTertiary :to="{name: 'username-profile', params: {username}}" class="w-full text-left">Profile</UiButtonsTertiary>
         </li>
         <li>
-          <UiButtonsTab class="w-full text-left">Log out</UiButtonsTab>
+          <UiButtonsTertiary :to="{name: 'username-buying-dashboard', params: {username}}" class="w-full text-left">Orders</UiButtonsTertiary>
+        </li>
+
+        <!-- Logout button -->
+        <li>
+          <UiButtonsTertiary @clicked="$emit('logout')" class="w-full text-left">Log out</UiButtonsTertiary>
         </li>
       </ul>
     </div>
   </div>
 </template>
-<script>
-export default {
-  props: {
-    menu_revealed: false,
-    signedIn: {
-      type: Boolean,
-      default: false
-    },
-    isSeller: {
-      type: Boolean,
-      default: false
-    },
-    username: {
-      default: 'testing',
-      type: String
-    },
-    profileImagePath: String
+
+<script setup>
+// Props with validation
+const props = defineProps({
+  menu_revealed: {
+    type: Boolean,
+    default: false
   },
-}
+  signedIn: {
+    type: Boolean,
+    default: false
+  },
+  isSeller: {
+    type: Boolean,
+    default: false
+  },
+  isCustomer: {
+    type: Boolean,
+    default: false
+  },
+  username: {
+    type: String,
+    default: 'user'
+  },
+  profileImagePath: String,
+  showMarketplaceLink: {
+    type: Boolean,
+    default: true
+  },
+  showCart: {
+    type: Boolean,
+    default: true
+  },
+  showRoleSwitch: {
+    type: Boolean,
+    default: true
+  }
+})
+
+// Emit events
+const emit = defineEmits(['cartClicked', 'logout'])
 </script>
