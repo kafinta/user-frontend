3f5c59e809363198acb449fdfdf3d94fcd3ec554
Quadir <aquadirmuhammad@gmail.com>
2025-05-12 18:01:08 +0100

    refactor: simplify Pie component by removing unnecessary data properties and using computed properties for value formatting

--- components/Chart/Pie.vue
+++ components/Chart/Pie.vue
@@ -2,19 +2,13 @@
   <div class="flex items-center justify-center">
     <div :style="gradientStyle" class="h-20 w-20 rounded-full p-1.5 radial bg-primary">
       <div class="bg-white rounded-full h-full w-full grid place-items-center">
-        <UiTypographyP>{{value}}{{ percent }}</UiTypographyP>
+        <UiTypographyP>{{ displayValue }}</UiTypographyP>
       </div>
     </div>
   </div>
 </template>
 <script>
 export default {
-  data(){
-    return {
-      percentage: this.value/this.max * 100,
-      percent: '%'
-    }
-  },
   props: {
     max: {
       default: 100,
@@ -24,19 +18,40 @@ export default {
       default: 50,
       type: Number
     },
-    isPercent: Boolean
+    isPercent: {
+      type: Boolean,
+      default: false
+    },
+    decimals: {
+      type: Number,
+      default: 0
+    }
   },
   computed: {
+    percentage() {
+      // Ensure we have valid numbers and handle edge cases
+      const val = Number(this.value) || 0;
+      const maximum = Number(this.max) || 100;
+      return (val / maximum) * 100;
+    },
+    formattedValue() {
+      // Ensure we have a valid number
+      const val = Number(this.value) || 0;
+      return this.decimals > 0 ? val.toFixed(this.decimals) : Math.round(val);
+    },
+    displayValue() {
+      return `${this.formattedValue}${this.isPercent ? '%' : ''}`;
+    },
     gradientStyle() {
       return {
         background: `conic-gradient(#C9B14F ${this.percentage}%, #eaeaea 0%)`
       };
     }
-  },
-  mounted(){
-    this.isPercent ? this.percent = '%' : this.percent = '';
   }
 }
 </script>
 <style>
+.radial {
+  background-clip: content-box;
+}
 </style>
