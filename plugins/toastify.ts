f62eeee0a5b2dfdeb2163f65aedfe0cd9e0cb4a0
Quadir <aquadirmuhammad@gmail.com>
2025-05-24 13:20:24 +0100

    refactor: move CSS import for Vue3Toastify to nuxt.config.ts for better organization

--- plugins/toastify.ts
+++ plugins/toastify.ts
@@ -1,6 +1,5 @@
 import { defineNuxtPlugin } from '#app'
 import Vue3Toastify, { toast, type ToastContainerOptions } from 'vue3-toastify'
-import 'vue3-toastify/dist/index.css'
 
 export default defineNuxtPlugin((nuxtApp) => {
   // Configure Vue3Toastify with default options
