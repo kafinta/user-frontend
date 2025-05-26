a03e159dca137063ff81663b29516c66c87cc029
Quadir <aquadirmuhammad@gmail.com>
2025-05-26 12:54:12 +0100

    refactor: update image rendering logic

--- components/Products/Image.vue
+++ components/Products/Image.vue
@@ -2,10 +2,11 @@
   <div v-if="imagePath" :style="{background:`url(${imagePath})`}" class="w-full product background"></div>
   <div v-else class="bg-accent-100 product w-full"></div>
 </template>
-<script>
-  export default{
-    props: {
-      imagePath: String
-    }
+<script setup>
+const props = defineProps({
+  imagePath: {
+    type: String,
+    default: ''
   }
+});
 </script>
