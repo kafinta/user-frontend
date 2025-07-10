a9bc2c04dab2e8f8e4f65922f24aba360f00d2a4
Quadir <aquadirmuhammad@gmail.com>
2025-07-10 18:54:13 +0100

    refactor(dropdown): add separator support in DropdownMenu for better item organization

--- components/Ui/DropdownMenu.vue
+++ components/Ui/DropdownMenu.vue
@@ -9,19 +9,24 @@
       <div v-if="open" class="absolute right-0 z-20 mt-2 w-44 rounded-md shadow-lg bg-white ring-1 ring-accent-200 focus:outline-none" @click.outside="closeMenu">
         <ul class="py-1">
           <li v-for="(item, idx) in items" :key="idx">
-            <button
-              :class="[
-                'w-full flex items-center gap-2 px-4 py-2 text-left transition-colors',
-                item.danger
-                  ? 'text-red-600 hover:bg-accent-100 focus:bg-accent-100'
-                  : 'text-accent-700 hover:bg-accent-100 focus:bg-accent-100'
-              ]"
-              @click="handleAction(item)"
-              :disabled="item.disabled"
-            >
-              <component v-if="item.icon" :is="item.icon" class="w-4 h-4" />
-              <span>{{ item.label }}</span>
-            </button>
+            <template v-if="item.separator">
+              <hr class="my-1 border-accent-200" />
+            </template>
+            <template v-else>
+              <button
+                :class="[
+                  'w-full flex items-center gap-2 px-4 py-2 text-left transition-colors',
+                  item.danger
+                    ? 'text-red-600 hover:bg-accent-100 focus:bg-accent-100'
+                    : 'text-accent-700 hover:bg-accent-100 focus:bg-accent-100'
+                ]"
+                @click="handleAction(item)"
+                :disabled="item.disabled"
+              >
+                <component v-if="item.icon" :is="item.icon" class="w-4 h-4" />
+                <span>{{ item.label }}</span>
+              </button>
+            </template>
           </li>
         </ul>
       </div>
