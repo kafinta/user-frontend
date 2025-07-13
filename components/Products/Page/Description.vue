cf030f603e4194ae82859eafc1db785604b38e06
Quadir <aquadirmuhammad@gmail.com>
2025-07-13 17:05:36 +0100

    refactor(description): update heading level and margin for improved layout consistency

--- components/Products/Page/Description.vue
+++ components/Products/Page/Description.vue
@@ -1,19 +1,16 @@
 <template>
-  <div class="py-5">
+  <div class="mt-2">
     <template v-if="isLoading">
       <UiSkeleton height="2rem" class="mb-3 w-1/2" />
       <UiSkeleton height="1rem" class="mb-1" v-for="i in 8" :key="i" />
     </template>
     <template v-else>
-      <UiTypographyH2>Product Description</UiTypographyH2>
-      <UiTypographyP>{{ product?.description || 'N/A' }}</UiTypographyP>
+      <UiTypographyH3>Product Description</UiTypographyH3>
+      <UiTypographyP class="mt-3">{{ product?.description || 'N/A' }}</UiTypographyP>
     </template>
   </div>
 </template>
 <script setup>
-import UiSkeleton from '~/components/Ui/Skeleton/Index.vue';
-import UiTypographyH2 from '~/components/Ui/Typography/H2.vue';
-import UiTypographyP from '~/components/Ui/Typography/P.vue';
 const props = defineProps({
   product: Object,
   isLoading: Boolean
