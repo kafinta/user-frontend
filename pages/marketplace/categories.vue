95e10fa3a3364313b96627ab2817848bd92657b3
Quadir <aquadirmuhammad@gmail.com>
2025-08-31 13:51:23 +0100

    refactor: update navigation buttons to use event handlers for category and location changes

--- pages/marketplace/categories.vue
+++ pages/marketplace/categories.vue
@@ -6,7 +6,7 @@
           <UiTypographyH2>{{ selectionMessage }}</UiTypographyH2>
           <UiBreadcrumbs :model="breadcrumbItems" />
         </div>
-        <UiButtonsPrimary :url="{ path: '/marketplace/locations', query: { ...route.query } }">Change Room</UiButtonsPrimary>
+        <UiButtonsPrimary @clicked="productFilters.changeLocation">Change Room</UiButtonsPrimary>
       </div>
       <ul class="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 mt-12">
         <li v-if="isLoading" v-for="n in 12" :key="n">
